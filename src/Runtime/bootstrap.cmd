67d03856c4ca6abc837947d911dbcc92850e488b
Andres Paz <anpaz@microsoft.com>
2019-06-20 14:20:49 -0700

    Creating ci build.

--- src/Runtime/bootstrap.cmd
+++ src/Runtime/bootstrap.cmd
@@ -6,9 +6,13 @@ cmake   -version || GOTO missingDependency
 echo.
 
 SET BUILD_FOLDER=build
+SET DROP_FOLDER=%BUILD_ARTIFACTSTAGINGDIRECTORY%\drop\src\simulation\Runtime\build
 
 IF NOT EXIST linux mkdir linux
+IF EXIST %DROP_FOLDER%\libMicrosoft.Quantum.Simulator.Runtime.so    copy %DROP_FOLDER%\libMicrosoft.Quantum.Simulator.Runtime.so     linux\Microsoft.Quantum.Simulator.Runtime.dll
+
 IF NOT EXIST osx mkdir osx
+IF EXIST %DROP_FOLDER%\libMicrosoft.Quantum.Simulator.Runtime.dylib copy %DROP_FOLDER%\libMicrosoft.Quantum.Simulator.Runtime.dylib  osx\Microsoft.Quantum.Simulator.Runtime.dll
 
 IF NOT EXIST %BUILD_FOLDER% mkdir %BUILD_FOLDER%
 pushd %BUILD_FOLDER%
