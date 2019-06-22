7ca7a9e9023003f923c3e2c918b96334ee1c9585
Andres Paz <anpaz@microsoft.com>
2019-06-22 09:43:40 -0700

    Removing copyright notice from rosslyn wrapper.

--- src/Simulation/RoslynWrapper/Common.fs
+++ src/Simulation/RoslynWrapper/Common.fs
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 namespace Microsoft.Quantum.RoslynWrapper
 
 #nowarn "1182"    // Unused parameters
