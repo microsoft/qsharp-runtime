950f9a72e328a7df725d5edd9dc33bdaa5fdea28
Bettina Heim <beheim@microsoft.com>
2019-12-20 17:32:45 -0800

    using dotnet pack for core

--- build/pack.ps1
+++ build/pack.ps1
@@ -34,11 +34,29 @@ function Pack-One() {
     }
 }
 
+function Pack-One-Dotnet() {
+    Param($project, $option1="", $option2="", $option3="")
+    dotnet pack $project `
+        -o $Env:NUGET_OUTDIR `
+        -c $Env:BUILD_CONFIGURATION `
+        -v detailed `
+        /property:DefineConstants=$Env:ASSEMBLY_CONSTANTS `
+        /property:Version=$Env:NUGET_VERSION `
+        $option1 `
+        $option2 `
+        $option3
+
+    if  ($LastExitCode -ne 0) {
+        Write-Host "##vso[task.logissue type=error;]Failed to pack $project."
+        $script:all_ok = $False
+    }
+}
+
 
 Write-Host "##[info]Using nuget to create packages"
 Pack-One '../src/Simulation/CsharpGeneration/Microsoft.Quantum.CsharpGeneration.fsproj' '-IncludeReferencedProjects'
-Pack-One '../src/Simulation/QsharpCore/Microsoft.Quantum.QSharp.Core.csproj'
-Pack-One '../src/Simulation/Core/Microsoft.Quantum.Runtime.Core.csproj'
+Pack-One-Dotnet '../src/Simulation/Core/Microsoft.Quantum.Runtime.Core.csproj'
+Pack-One-Dotnet '../src/Simulation/QsharpCore/Microsoft.Quantum.QSharp.Core.csproj'
 #Pack-One '../src/Simulation/Simulators/Microsoft.Quantum.Simulators.csproj' '-IncludeReferencedProjects'  FIXME: WE NEED TO PACK THIS!
 Pack-One '../src/Microsoft.Quantum.Development.Kit.nuspec'
 Pack-One '../src/Xunit/Microsoft.Quantum.Xunit.csproj'
