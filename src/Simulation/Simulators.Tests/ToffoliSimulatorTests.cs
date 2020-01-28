00818415e46a90d880f4b4cc11a4f89ab88ff8ff
Irina Yatsenko <36858951+irinayat-MS@users.noreply.github.com>
2020-01-28 10:33:28 -0800

    fix mismatch in line break format between oracle and result in ToffoliDumpState test (#133)

--- src/Simulation/Simulators.Tests/ToffoliSimulatorTests.cs
+++ src/Simulation/Simulators.Tests/ToffoliSimulatorTests.cs
@@ -222,6 +222,10 @@ namespace Microsoft.Quantum.Simulation.Simulators.Tests
         [Fact]
         public void ToffoliDumpState()
         {
+            var NL = System.Environment.NewLine;
+            var expectedHeader = "Offset  \tState Data" + NL +
+                                 "========\t==========" + NL;
+
             var sim = new ToffoliSimulator();
 
             var allocate = sim.Get<Intrinsic.Allocate>();
@@ -251,32 +255,20 @@ namespace Microsoft.Quantum.Simulation.Simulators.Tests
             var testPath = Path.GetTempFileName();
             output.WriteLine($"Dumping machine to {testPath}.");
             dumpMachine.Apply(testPath);
-            var expected = @"Offset  \tState Data
-========\t==========
-00000000\t1001001001001
-"
-                .Replace("\\t", "\t");
-            Assert.Equal(expected, File.ReadAllText(testPath));
+            var expectedAutomatic = expectedHeader + "00000000\t1001001001001" + NL;
+            Assert.Equal(expectedAutomatic, File.ReadAllText(testPath));
 
             sim.DumpFormat = ToffoliDumpFormat.Bits;
             testPath = Path.GetTempFileName();
             dumpMachine.Apply(testPath);
-            expected = @"Offset  \tState Data
-========\t==========
-00000000\t1001001001001
-"
-                .Replace("\\t", "\t");
-            Assert.Equal(expected, File.ReadAllText(testPath));
+            var expectedBits = expectedHeader + "00000000\t1001001001001" + NL;
+            Assert.Equal(expectedBits, File.ReadAllText(testPath));
 
             sim.DumpFormat = ToffoliDumpFormat.Hex;
             testPath = Path.GetTempFileName();
             dumpMachine.Apply(testPath);
-            expected = @"Offset  \tState Data
-========\t==========
-00000000\t49 12
-"
-                .Replace("\\t", "\t");
-            Assert.Equal(expected, File.ReadAllText(testPath));
+            var expectedHex = expectedHeader + "00000000\t49 12" + NL;
+            Assert.Equal(expectedHex, File.ReadAllText(testPath));
 
             // Reset and return our qubits for the next example.
             Prepare(qubits);
@@ -291,35 +283,26 @@ namespace Microsoft.Quantum.Simulation.Simulators.Tests
             sim.DumpFormat = ToffoliDumpFormat.Automatic;
             testPath = Path.GetTempFileName();
             dumpMachine.Apply(testPath);
-            expected = @"Offset  \tState Data
-========\t==========
-00000000\t49 92 24 49 92 24 49 92
-"
-                .Replace("\\t", "\t");
-            Assert.Equal(expected, File.ReadAllText(testPath));
+            var expectedAutomaticLarge =
+                    expectedHeader + "00000000\t49 92 24 49 92 24 49 92" + NL;
+            Assert.Equal(expectedAutomaticLarge, File.ReadAllText(testPath));
 
             sim.DumpFormat = ToffoliDumpFormat.Bits;
             testPath = Path.GetTempFileName();
             dumpMachine.Apply(testPath);
-            expected = @"Offset  \tState Data
-========\t==========
-00000000\t1001001001001001
-00000002\t0010010010010010
-00000004\t0100100100100100
-00000006\t1001001001001001
-"
-                .Replace("\\t", "\t");
-            Assert.Equal(expected, File.ReadAllText(testPath));
+            var expectedBitsLarge = expectedHeader +
+                                    "00000000\t1001001001001001" + NL +
+                                    "00000002\t0010010010010010" + NL +
+                                    "00000004\t0100100100100100" + NL +
+                                    "00000006\t1001001001001001" + NL;
+            Assert.Equal(expectedBitsLarge, File.ReadAllText(testPath));
 
             sim.DumpFormat = ToffoliDumpFormat.Hex;
             testPath = Path.GetTempFileName();
             dumpMachine.Apply(testPath);
-            expected = @"Offset  \tState Data
-========\t==========
-00000000\t49 92 24 49 92 24 49 92
-"
-                .Replace("\\t", "\t");
-            Assert.Equal(expected, File.ReadAllText(testPath));
+            var expectedHexLarge =
+                    expectedHeader + "00000000\t49 92 24 49 92 24 49 92" + NL;
+            Assert.Equal(expectedHexLarge, File.ReadAllText(testPath));
 
             // Reset and return our qubits for the next example.
             Prepare(qubits);
