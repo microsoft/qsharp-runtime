a52b3dda4d44eff0cfae178462a56b4ac4373447
Bettina Heim <beheim@microsoft.com>
2020-03-09 20:33:27 -0700

    accidentally commit something I didn't mean to

--- src/Simulation/Core/OperationInfo.cs
+++ src/Simulation/Core/OperationInfo.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
 
 namespace Microsoft.Quantum.Simulation.Core
 {
@@ -18,7 +17,5 @@ namespace Microsoft.Quantum.Simulation.Core
         {
             this.Operation = operation;
         }
-
-        public IReadOnlyDictionary<> TargetSpecificRedirects; 
     }
 }
