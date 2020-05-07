c363cc38856754bb5b382c6698865ad7a5dfd3e9
Sarah Marshall <sarah.marshall@microsoft.com>
2020-05-07 13:58:15 -0700

    Add copyright headers

--- src/Simulation/EntryPointDriver/Azure.cs
+++ src/Simulation/EntryPointDriver/Azure.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 using System.CommandLine.Parsing;
 using System.Threading.Tasks;
 using Microsoft.Quantum.Runtime;
