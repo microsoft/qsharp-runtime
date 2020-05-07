f4335c4dd9ecdecd6ee038e280cd395b8c823c32
Sarah Marshall <sarah.marshall@microsoft.com>
2020-05-06 18:23:29 -0700

    Use real provider for Azure submission

---
+++ src/Simulation/EntryPointDriver/Azure.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.CommandLine.Parsing;
+using System.Threading.Tasks;
+using Microsoft.Quantum.Runtime;
+
+namespace Microsoft.Quantum.CsharpGeneration.EntryPointDriver
+{
+    /// <summary>
+    /// Provides entry point submission to Azure Quantum.
+    /// </summary>
+    internal static class Azure
+    {
+        /// <summary>
+        /// Submits the entry point to Azure Quantum.
+        /// </summary>
+        /// <param name="entryPoint">The entry point.</param>
+        /// <param name="parseResult">The command-line parsing result.</param>
+        /// <param name="settings">The submission settings.</param>
+        /// <typeparam name="TIn">The entry point's argument type.</typeparam>
+        /// <typeparam name="TOut">The entry point's return type.</typeparam>
+        internal static async Task Submit<TIn, TOut>(
+            IEntryPoint<TIn, TOut> entryPoint, ParseResult parseResult, AzureSettings settings)
+        {
+            // TODO: DefaultIfShadowed for settings
+            var machineType = Type.GetType(
+                "Microsoft.Quantum.Providers.IonQ.Targets.IonQQuantumMachine, Microsoft.Quantum.Providers.IonQ",
+                throwOnError: true);
+            var machine = (IQuantumMachine)Activator.CreateInstance(
+                machineType, settings.Target, settings.Storage, settings.ToWorkspace());
+            var output = await machine.ExecuteAsync(entryPoint.Info, entryPoint.CreateArgument(parseResult));
+            // TODO: Provide output options and show the most frequent output by default. 
+            foreach (var (result, frequency) in output.Histogram)
+            {
+                Console.WriteLine($"{result} (frequency = {frequency})");
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Settings for a submission to Azure Quantum.
+    /// </summary>
+    internal sealed class AzureSettings
+    {
+        /// <summary>
+        /// The target device ID.
+        /// </summary>
+        public string? Target { get; set; }
+        
+        /// <summary>
+        /// The Azure subscription ID.
+        /// </summary>
+        public string? Subscription { get; set; }
+        
+        /// <summary>
+        /// The Azure resource group name.
+        /// </summary>
+        public string? ResourceGroup { get; set; }
+        
+        /// <summary>
+        /// The Azure workspace name.
+        /// </summary>
+        public string? Workspace { get; set; }
+        
+        /// <summary>
+        /// The Azure storage account connection string.
+        /// </summary>
+        public string? Storage { get; set; }
+
+        /// <summary>
+        /// Converts these settings into a Microsoft.Azure.Quantum.Workspace object.
+        /// </summary>
+        /// <returns>The workspace object corresponding to these settings.</returns>
+        internal object ToWorkspace()
+        {
+            var workspaceType = Type.GetType(
+                "Microsoft.Azure.Quantum.Workspace, Microsoft.Azure.Quantum.Client", throwOnError: true);
+            var tokenCredentialType = Type.GetType("Azure.Core.TokenCredential, Azure.Core", throwOnError: true);
+            var constructor = workspaceType.GetConstructor(new[]
+                { typeof(string), typeof(string), typeof(string), tokenCredentialType, typeof(Uri) });
+            return constructor.Invoke(new object?[] { Subscription, ResourceGroup, Workspace, null, null });
+        }
+    }
+}
--- src/Simulation/EntryPointDriver/Driver.cs
+++ src/Simulation/EntryPointDriver/Driver.cs
@@ -10,10 +10,8 @@ using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Azure.Quantum;
 using Microsoft.Quantum.QsCompiler.ReservedKeywords;
 using Microsoft.Quantum.Simulation.Core;
-using Microsoft.Quantum.Simulation.Simulators;
 using static Microsoft.Quantum.CsharpGeneration.EntryPointDriver.Driver;
 
 namespace Microsoft.Quantum.CsharpGeneration.EntryPointDriver
@@ -21,6 +19,9 @@ namespace Microsoft.Quantum.CsharpGeneration.EntryPointDriver
     /// <summary>
     /// The entry point driver is the entry point for the C# application that executes the Q# entry point.
     /// </summary>
+    /// <typeparam name="TCallable">The entry point's callable type.</typeparam>
+    /// <typeparam name="TIn">The entry point's argument type.</typeparam>
+    /// <typeparam name="TOut">The entry point's return type.</typeparam>
     public sealed class Driver<TCallable, TIn, TOut> where TCallable : AbstractCallable, ICallable
     {
         /// <summary>
@@ -95,28 +96,7 @@ namespace Microsoft.Quantum.CsharpGeneration.EntryPointDriver
         private async Task<int> Simulate(ParseResult parseResult, string simulator)
         {
             simulator = DefaultIfShadowed(SimulatorOptions.First(), simulator, entryPoint.DefaultSimulator);
-            if (simulator == AssemblyConstants.ResourcesEstimator)
-            {
-                var resourcesEstimator = new ResourcesEstimator();
-                await resourcesEstimator.Run<TCallable, TIn, TOut>(entryPoint.CreateArgument(parseResult));
-                Console.WriteLine(resourcesEstimator.ToTSV());
-            }
-            else
-            {
-                var (isCustom, createSimulator) =
-                    simulator == AssemblyConstants.QuantumSimulator
-                        ? (false, () => new QuantumSimulator())
-                        : simulator == AssemblyConstants.ToffoliSimulator
-                        ? (false, new Func<IOperationFactory>(() => new ToffoliSimulator()))
-                        : (true, entryPoint.CreateDefaultCustomSimulator);
-                if (isCustom && simulator != entryPoint.DefaultSimulator)
-                {
-                    DisplayCustomSimulatorError(simulator);
-                    return 1;
-                }
-                await RunSimulator(parseResult, createSimulator);
-            }
-            return 0;
+            return await Simulation<TCallable, TIn, TOut>.Simulate(entryPoint, parseResult, simulator);
         }
 
         /// <summary>
@@ -124,41 +104,10 @@ namespace Microsoft.Quantum.CsharpGeneration.EntryPointDriver
         /// </summary>
         /// <param name="parseResult">The command-line parsing result.</param>
         /// <param name="settings">The submission settings.</param>
-        private static void Submit(ParseResult parseResult, AzureSettings settings)
-        {
-            // TODO
-            Console.WriteLine($"Target: {settings.Target}");
-            Console.WriteLine($"Subscription: {settings.Subscription}");
-            Console.WriteLine($"ResourceGroup: {settings.ResourceGroup}");
-            Console.WriteLine($"Workspace: {settings.Workspace}");
-            Console.WriteLine($"Storage: {settings.Storage}");
-        }
-
-        /// <summary>
-        /// Runs the entry point on a simulator and displays its return value.
-        /// </summary>
-        /// <param name="parseResult">The command-line parsing result.</param>
-        /// <param name="createSimulator">A function that creates an instance of the simulator to use.</param>
-        private async Task RunSimulator(ParseResult parseResult, Func<IOperationFactory> createSimulator)
-        {
-            var simulator = createSimulator();
-            try
-            {
-                var value = await simulator.Run<TCallable, TIn, TOut>(entryPoint.CreateArgument(parseResult));
-                if (!(value is QVoid))
-                {
-                    Console.WriteLine(value);
-                }
-            }
-            finally
-            {
-                if (simulator is IDisposable disposable)
-                {
-                    disposable.Dispose();
-                }
-            }
-        }
-
+        private async Task Submit(ParseResult parseResult, AzureSettings settings) =>
+            // TODO: DefaultIfShadowed
+            await Azure.Submit(entryPoint, parseResult, settings);
+        
         /// <summary>
         /// Returns true if the alias is not already used by an entry point option.
         /// </summary>
@@ -242,25 +191,6 @@ namespace Microsoft.Quantum.CsharpGeneration.EntryPointDriver
                 command.AddOption(suggestions is null || !suggestions.Any()
                     ? option
                     : option.WithSuggestions(suggestions)));
-
-        /// <summary>
-        /// Displays an error message for using a non-default custom simulator.
-        /// </summary>
-        /// <param name="name">The name of the custom simulator.</param>
-        private static void DisplayCustomSimulatorError(string name)
-        {
-            var originalForeground = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Error.WriteLine($"The simulator '{name}' could not be found.");
-            Console.ForegroundColor = originalForeground;
-            Console.Error.WriteLine();
-            Console.Error.WriteLine(
-                $"If '{name}' is a custom simulator, it must be set in the DefaultSimulator project property:");
-            Console.Error.WriteLine();
-            Console.Error.WriteLine("<PropertyGroup>");
-            Console.Error.WriteLine($"  <DefaultSimulator>{name}</DefaultSimulator>");
-            Console.Error.WriteLine("</PropertyGroup>");
-        }
     }
 
     /// <summary>
@@ -278,43 +208,6 @@ namespace Microsoft.Quantum.CsharpGeneration.EntryPointDriver
         });
     }
 
-    /// <summary>
-    /// Settings for a submission to Azure Quantum.
-    /// </summary>
-    internal sealed class AzureSettings
-    {
-        /// <summary>
-        /// The target device ID.
-        /// </summary>
-        public string? Target { get; set; }
-        
-        /// <summary>
-        /// The Azure subscription ID.
-        /// </summary>
-        public string? Subscription { get; set; }
-        
-        /// <summary>
-        /// The Azure resource group name.
-        /// </summary>
-        public string? ResourceGroup { get; set; }
-        
-        /// <summary>
-        /// The Azure workspace name.
-        /// </summary>
-        public string? Workspace { get; set; }
-        
-        /// <summary>
-        /// The Azure storage account connection string.
-        /// </summary>
-        public string? Storage { get; set; }
-        
-        /// <summary>
-        /// Converts these settings into a <see cref="Workspace"/>.
-        /// </summary>
-        /// <returns>The <see cref="Workspace"/> corresponding to these settings.</returns>
-        internal Workspace ToWorkspace() => new Workspace(Subscription, ResourceGroup, Workspace, Storage);
-    }
-    
     /// <summary>
     /// A modification of the command-line <see cref="HelpBuilder"/> class.
     /// </summary>
