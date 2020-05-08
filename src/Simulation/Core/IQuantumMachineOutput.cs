97875902f0c7fd0aaafa574302fc30327398a226
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2020-05-08 16:46:57 -0700

    Removed type parameters from IQuantumMachine interface. (#185) (#195)
    * Removed type parameters from IQuantumMachine interface. (#185)
    
    * Removed type parameters from IQuantumMachine interface.
    
    * Addressed Sarah's feedback.
    
    * Update src/Simulation/Core/IQuantumMachineOutput.cs
    
    Co-authored-by: Sarah Marshall <33814365+samarsha@users.noreply.github.com>
    
    Co-authored-by: Sarah Marshall <33814365+samarsha@users.noreply.github.com>
    (cherry picked from commit 1b53a81693d15189d734de2d6400d0691c1595b0)
    
    * Update src/Simulation/Core/IQuantumMachineJob.cs
    
    Co-authored-by: Sarah Marshall <33814365+samarsha@users.noreply.github.com>
    
    Co-authored-by: Sarah Marshall <33814365+samarsha@users.noreply.github.com>

---
+++ src/Simulation/Core/IQuantumMachineOutput.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Quantum.Runtime
+{
+    /// <summary>
+    /// Interface to access the results of a program executed in a quantum machine.
+    /// <typeparam name="TOutput">Type of output the quantum program returns.</typeparam>
+    /// </summary>
+    public interface IQuantumMachineOutput<TOutput>
+    {
+        /// <summary>
+        /// Gets a histogram of outputs.
+        /// The key is the output and the value is its frequency.
+        /// </summary>
+        IReadOnlyDictionary<TOutput, double> Histogram { get; }
+
+        /// <summary>
+        /// Gets the job associated to the output.
+        /// </summary>
+        IQuantumMachineJob Job { get; }
+
+        /// <summary>
+        /// Gets the number of times the program was executed.
+        /// </summary>
+        int Shots { get; }
+    }
+}
