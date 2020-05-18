874821f650e8ad1fb5686593818c58fffaab77c1
Ryan Shaffer <rmshaffer@gmail.com>
2020-05-18 17:36:42 -0400

    Allow reflection to find type in loaded assembly

--- src/Azure/Azure.Quantum.Client/Machine/QuantumMachineFactory.cs
+++ src/Azure/Azure.Quantum.Client/Machine/QuantumMachineFactory.cs
@@ -1,8 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#nullable enable
+
 using System;
-using System.Threading.Tasks;
+using System.Linq;
 using Microsoft.Quantum.Runtime;
 
 namespace Microsoft.Azure.Quantum
@@ -16,7 +18,7 @@ namespace Microsoft.Azure.Quantum
         /// <param name="targetName">The execution target for job submission.</param>
         /// <param name="storageAccountConnectionString">The connection string for the Azure storage account.</param>
         /// <returns>A quantum machine for job submission targeting <c>targetName</c>.</returns>
-        public static IQuantumMachine? CreateMachine(Workspace workspace, string targetName, string storageAccountConnectionString)
+        public static IQuantumMachine? CreateMachine(IWorkspace workspace, string targetName, string storageAccountConnectionString)
         {
             if (string.IsNullOrEmpty(targetName))
             {
@@ -25,13 +27,13 @@ namespace Microsoft.Azure.Quantum
 
             if (targetName.StartsWith("ionq."))
             {
-                var ionQType = Type.GetType(
-                    "Microsoft.Quantum.Providers.IonQ.Targets.IonQQuantumMachine, Microsoft.Quantum.Providers.IonQ",
-                    throwOnError: true);
+                var ionQType = AppDomain.CurrentDomain.GetAssemblies()
+                    .First(a => a.FullName.StartsWith("Microsoft.Quantum.Providers.IonQ,"))
+                    .GetType("Microsoft.Quantum.Providers.IonQ.Targets.IonQQuantumMachine", throwOnError: true);
                 return (IQuantumMachine)Activator.CreateInstance(
                     ionQType, targetName, storageAccountConnectionString, workspace);
             }
-            
+
             return null;
         }
     }
