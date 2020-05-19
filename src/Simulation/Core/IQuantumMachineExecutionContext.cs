db6125024217b21d1e805035f72a4be26a970b0d
Cesar Zaragoza Cortes <cesar.zaragoza@outlook.com>
2020-05-19 12:41:44 -0700

    Added submission and execution context interfaces.

---
+++ src/Simulation/Core/IQuantumMachineExecutionContext.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Quantum.Runtime
+{
+    public interface IQuantumMachineExecutionContext
+    {
+        int PollingInterval { get; set; }
+        int Timeout { get; set; }
+    }
+}
---
+++ src/Simulation/Core/IQuantumMachineSubmissionContext.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Quantum.Runtime
+{
+    public interface IQuantumMachineSubmissionContext
+    {
+        string FriendlyName { get; set; }
+        int Shots { get; set; }
+    }
+}
