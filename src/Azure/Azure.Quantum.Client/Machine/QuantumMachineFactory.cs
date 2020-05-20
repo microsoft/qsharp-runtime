1b70dc2070cbff35cdf487c8ef2f2256c9fc1afd
Ryan Shaffer <ryan.shaffer@microsoft.com>
2020-05-19 20:22:03 -0400

    Allow reflection to find type in loaded assembly (#205)
    * Update reflection to look for signed assembly first
    
    * Use IWorkspace instead of Workspace
    
    * Catch exceptions from GetType call

--- src/Azure/Azure.Quantum.Client/Machine/QuantumMachineFactory.cs
+++ src/Azure/Azure.Quantum.Client/Machine/QuantumMachineFactory.cs
@@ -15,23 +15,37 @@ namespace Microsoft.Azure.Quantum
         /// <param name="targetName">The execution target for job submission.</param>
         /// <param name="storageAccountConnectionString">The connection string for the Azure storage account.</param>
         /// <returns>A quantum machine for job submission targeting <c>targetName</c>.</returns>
-        public static IQuantumMachine? CreateMachine(Workspace workspace, string targetName, string storageAccountConnectionString)
+        public static IQuantumMachine? CreateMachine(IWorkspace workspace, string targetName, string storageAccountConnectionString)
         {
-            if (string.IsNullOrEmpty(targetName))
-            {
-                return null;
-            }
-
             var machineName =
-                targetName.StartsWith("ionq.")
+                targetName is null
+                ? null
+                : targetName.StartsWith("ionq.")
                 ? "Microsoft.Quantum.Providers.IonQ.Targets.IonQQuantumMachine, Microsoft.Quantum.Providers.IonQ"
                 : targetName.StartsWith("honeywell.")
                 ? "Microsoft.Quantum.Providers.Honeywell.Targets.HoneywellQuantumMachine, Microsoft.Quantum.Providers.Honeywell"
                 : null;
-            return machineName is null
+
+            Type machineType = null;
+            if (machineName != null)
+            {
+                // First try to load the signed assembly with the correct version, then try the unsigned one.
+                try
+                {
+                    machineType = Type.GetType($"{machineName}, Version={typeof(IWorkspace).Assembly.GetName().Version}, Culture=neutral, PublicKeyToken=40866b40fd95c7f5");
+                }
+                catch
+                {
+                    machineType = null;
+                }
+
+                machineType ??= Type.GetType(machineName, throwOnError: true);
+            }
+
+            return machineType is null
                 ? null
                 : (IQuantumMachine)Activator.CreateInstance(
-                    Type.GetType(machineName, throwOnError: true),
+                    machineType,
                     targetName,
                     storageAccountConnectionString,
                     workspace);
