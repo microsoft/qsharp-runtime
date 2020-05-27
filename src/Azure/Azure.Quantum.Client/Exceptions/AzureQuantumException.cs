f7e97f9c5bafa21de06a637138b722878ea36572
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2020-05-27 08:22:19 -0700

    Created AzzureQuantumException. (#216)
    This change adds AzureQuantumException which is the base exception to throw or create new exceptions related to the Azure quantum service or storage.

---
+++ src/Azure/Azure.Quantum.Client/Exceptions/AzureQuantumException.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Microsoft.Azure.Quantum.Exceptions
+{
+    public class AzureQuantumException : Exception
+    {
+        public AzureQuantumException(string message = "An exception related to the Azure quantum service or storage occurred.")
+            : base(message)
+        {
+        }
+    }
+}
