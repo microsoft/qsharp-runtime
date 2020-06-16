00cb39024a4ac0067fa57d11216e419604a4a835
Dave Wecker <wecker@microsoft.com>
2020-06-15 21:24:33 -0700

    .

--- src/Simulation/Native/src/external/fused.hpp
+++ src/Simulation/Native/src/external/fused.hpp
@@ -127,7 +127,7 @@ class Fused
       dbgElapsed = elapsed.count();
       if (dbgElapsed >= 10.0) {
             double nFused = (float)dbgNfused;
-            printf("@@@DBG sz=%.2f nQs=%.2f nCs=%.2f flushes=%8.2g gates=%8.2g  elap=%5.1f gps=%10.4g (fus=%5.1f%%, ker=%5.1f%%)\n",
+            printf("@@@DBG sz=%.2f nQs=%.2f nCs=%.2f flsh=%8.2g gts=%8.2g elap=%5.1f gps=%10.4g (fus=%5.1f%%, ker=%5.1f%%)\n",
                 ((float)dbgSize / nFused),
                 ((float)dbgNqs / nFused),
                 ((float)dbgNcs / nFused),
