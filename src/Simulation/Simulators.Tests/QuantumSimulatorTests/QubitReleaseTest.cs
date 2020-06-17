fe2a1aa710f906b6e48c0dd737f6fcfeb59b0d5c
Aniket Dalvi <t-anikda@microsoft.com>
2020-06-17 14:21:46 -0400

    Added test for checking state of reallocated qubit

--- src/Simulation/Simulators.Tests/QuantumSimulatorTests/QubitReleaseTest.cs
+++ src/Simulation/Simulators.Tests/QuantumSimulatorTests/QubitReleaseTest.cs
@@ -32,5 +32,33 @@ namespace Microsoft.Quantum.Simulation.Simulators.Tests
             //should not throw an exception, as Measured qubits are allowed to be released, and the release aspect is handled in the C++ code
             await ReleaseMeasuredQubitCheck.Run(sim);
         }
+
+        //test to check that qubit that is released and reallocated is in state |0>
+        [Fact]
+        public async Task ReallocateQubitInGroundStateTest()
+        {
+            var sim = new QuantumSimulator();
+            var allocate = sim.Get<Intrinsic.Allocate>();
+            var release = sim.Get<Intrinsic.Release>();
+            var q1 = allocate.Apply(1);
+            var q1Id = q1[0].Id;
+            var gate = sim.Get<Intrinsic.X>();
+            var measure = sim.Get<Intrinsic.M>();
+            gate.Apply(q1[0]);
+            var result1 = measure.Apply(q1[0]);
+            //Check X operation
+            Assert.Equal(result1, Result.One);
+            release.Apply(q1[0]);
+            var q2 = allocate.Apply(1);
+            var q2Id = q2[0].Id;
+            //Assert reallocated qubit has the same id as the one released
+            Assert.Equal(q1Id, q2Id);
+            var result2 = measure.Apply(q2[0]);
+            //Assert reallocated qubit has is initialized in state |0>
+            Assert.Equal(result2, Result.Zero);
+
+
+            
+        }
     }
 }
