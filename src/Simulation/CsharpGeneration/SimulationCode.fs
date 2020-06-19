20257a00ed71df0255c83f9a9d5d69b967503088
Scott Carda <sccarda@microsoft.com>
2020-06-18 17:54:08 -0700

    Fixed an issue with the C# generation for UDT classes where tuple fields of the UDT where not being correctly parsed.

--- src/Simulation/CsharpGeneration/SimulationCode.fs
+++ src/Simulation/CsharpGeneration/SimulationCode.fs
@@ -1454,17 +1454,24 @@ module SimulationCode =
                     []
                 ``}``
             :> MemberDeclarationSyntax
-
-        let buildNamedItemFields = 
-            let items = getAllItems (``ident`` "Data") qsharpType
-            let rec buildProps = function 
-                | QsTuple items -> items |> Seq.collect (fun i -> buildProps i)
-                | QsTupleItem (Anonymous _) -> items.Dequeue() |> ignore; Seq.empty
-                | QsTupleItem (Named decl) -> seq { yield
-                    ``property-arrow_get`` (roslynTypeName context decl.Type) decl.VariableName.Value [ ``public`` ] 
-                        ``get`` (``=>`` (items.Dequeue()))
-                    :> MemberDeclarationSyntax}
-            buildProps udt.TypeItems |> Seq.toList
+        let buildNamedItemFields =
+            let produceProperty (decl : LocalVariableDeclaration<NonNullable<_>>) valueExpr =
+                ``property-arrow_get`` (roslynTypeName context decl.Type) decl.VariableName.Value [ ``public`` ] 
+                    ``get`` (``=>`` valueExpr) :> MemberDeclarationSyntax
+            let rec buildProps current = function
+                | QsTuple items -> items |> Seq.mapi (fun i x -> buildProps (current <|.|> ``ident`` ("Item" + (i+1).ToString())) x) |> Seq.collect id
+                | QsTupleItem (Anonymous _) -> Seq.empty
+                | QsTupleItem (Named decl) -> seq { yield produceProperty decl current }
+            // UDT types are packaged differently if there is one constituent type, or many.
+            // This function handles that difference in packaging.
+            let rec readType typeItem =
+                match typeItem with
+                | QsTuple items when items.IsEmpty -> Seq.empty
+                | QsTuple items when items.Length = 1 -> items |> Seq.head |> readType
+                | QsTuple _ -> buildProps (``ident`` "Data") typeItem
+                | QsTupleItem (Anonymous _) -> Seq.empty
+                | QsTupleItem (Named decl) -> seq { yield produceProperty decl (``ident`` "Data") }
+            readType udt.TypeItems |> Seq.toList
         let buildItemFields = 
             let buildOne i t =
                 ``property-arrow_get`` (roslynTypeName context t) (sprintf "Item%d" (i+1)) [ ``public`` ] 
