60b6f39306b53063e41bdf3601a37f2e35fef0c6
Mathias Soeken <mathias.soeken@outlook.com>
2020-06-22 10:51:45 +0200

    Test case for rotations in Toffoli Simulator.

--- src/Simulation/Simulators.Tests/ToffoliSimulatorTests.cs
+++ src/Simulation/Simulators.Tests/ToffoliSimulatorTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using Xunit;
@@ -141,6 +141,15 @@ namespace Microsoft.Quantum.Simulation.Simulators.Tests
             var sim = new ToffoliSimulator();
 
             await Circuits.SwapTest.Run(sim);
+        }
+
+        [Fact]
+        public async Task ToffoliRotations()
+        {
+            var sim = new ToffoliSimulator();
+
+            var result = await Circuits.IncrementWithRotationsTest.Run(sim, 4);
+            Assert.Equal(5, result);
         }
 
         [Fact]
