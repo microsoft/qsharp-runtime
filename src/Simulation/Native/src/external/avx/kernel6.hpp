d3644f5e9f87b46050c3cf3ec578214820335ffe
Stefan J. Wernli <swernli@microsoft.com>
2020-06-23 09:54:34 -0700

    Merging Linux simulator update from dbwz8/simDbg (#281)
    * Merging Linux simulator update from dbwz8/simDbg
    
    * Remove extra semicolon

--- src/Simulation/Native/src/external/avx/kernel6.hpp
+++ src/Simulation/Native/src/external/avx/kernel6.hpp
@@ -212,7 +212,7 @@ void kernel(V& psi, unsigned id5, unsigned id4, unsigned id3, unsigned id2, unsi
 
 #ifndef _MSC_VER
 	if (ctrlmask == 0){
-		#pragma omp parallel for collapse(LOOP_COLLAPSE6) schedule(static)
+		#pragma omp parallel for collapse(LOOP_COLLAPSE6) schedule(static) proc_bind(spread)
 		for (std::size_t i0 = 0; i0 < n; i0 += 2 * dsorted[0]){
 			for (std::size_t i1 = 0; i1 < dsorted[0]; i1 += 2 * dsorted[1]){
 				for (std::size_t i2 = 0; i2 < dsorted[1]; i2 += 2 * dsorted[2]){
