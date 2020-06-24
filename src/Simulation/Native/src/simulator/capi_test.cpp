e1d0e927f45f1522bd7494d11518335b965fe3af
Dave Wecker <wecker@microsoft.com>
2020-06-24 11:15:01 -0700

    Working on getting clustered Shor to work

--- src/Simulation/Native/src/simulator/capi_test.cpp
+++ src/Simulation/Native/src/simulator/capi_test.cpp
@@ -5,15 +5,20 @@
 #include <cassert>
 #include <cmath>
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <complex>
 #include <array>
 #include <omp.h>
 #include <chrono>
+#include <regex>
+#include <string>
+#include <vector>
 
 #include "util/cpuid.hpp" //@@@DBG
-
 #include "capi.hpp"
+
+using namespace std;
 
 // some convenience functions
 void CX(unsigned sim_id, unsigned c, unsigned q)
@@ -55,7 +60,6 @@ void dump(unsigned sim_id, const char* label)
     std::cout << "]\n";
     Dump(sim_id, dump_callback);
 }
-
 
 void test_teleport()
 {
@@ -105,337 +109,72 @@ void test_teleport()
     destroy(sim_id);
 }
 
-void test_gates()
-{
-    auto sim_id = init();
-
-    allocateQubit(sim_id, 0);
-    allocateQubit(sim_id, 1);
-
-     CRx(sim_id, 1.0, 0, 1);
-
-    assert(M(sim_id, 1)==false);
-
-    X(sim_id, 0);
-     CRx(sim_id, 1.0, 0, 1);
-
-    H(sim_id, 1);
-    CRx(sim_id, -1.0, 0, 1);
-    H(sim_id, 1);
-
-    assert(M(sim_id, 1)==false);
-
-    X(sim_id, 1);
-
-    assert(M(sim_id, 1)==true);
-
-    X(sim_id, 1);
-
-    release(sim_id, 0);
-    release(sim_id, 1);
-
-    destroy(sim_id);
-}
-
-
-void test_allocate()
-{
-    auto sim_id = init();
-
-    allocateQubit(sim_id,0);
-    allocateQubit(sim_id,1);
-    allocateQubit(sim_id,2);
-    release(sim_id,1);
-    allocateQubit(sim_id,1);
-
-    assert(num_qubits(sim_id) == 3);
-
-    release(sim_id,0);
-    release(sim_id,1);
-    release(sim_id,2);
-
-    assert(num_qubits(sim_id) == 0);
-    destroy(sim_id);
-
-}
-/*
-// We can't use a lambda with captures to pass to a callback with __stdcall signature,
-// so we use a global variable/function for the check_state callback:
-using wavefunction_type = std::vector<std::complex<double>>;
-wavefunction_type _state;
-bool check_state(size_t idx, double r, double i) {
-    auto tol = 1e-5;
-    if (!((abs(_state[idx].real() - r) <= tol) && (abs(_state[idx].imag() - i) <= tol))) throw 1;
-    return true;
-}
-
-
-void test_dump()
-{
-  auto no_checks = [](size_t idx, double r, double i) { return true; };
-  auto sim_id = init();
-
-  unsigned int qs[3];
-  qs[0] = 0;
-  qs[1] = 1;
-  qs[2] = 2;
-
-  allocateQubit(sim_id, 0);
-  allocateQubit(sim_id, 1);
-  allocateQubit(sim_id, 2);
-
-  // simple check:
-  _state = wavefunction_type{ 1., 0., 0., 0., 0., 0., 0., 0. };
-  dump(sim_id, "dump-start");
-  Dump(sim_id, check_state);
-
-  // Check an invalid state will throw.
-  _state = wavefunction_type{ 0., 0., 0., 0., 0., 0., 0., 0. };
-  dump(sim_id, "dump-01");
-  try {
-    Dump(sim_id, check_state);
-    assert(false);
-  }
-  catch (int n) {
-      std::cerr << n << std::endl;
-    assert(n == 1);
-  }
-
-  // change state
-  X(sim_id, 0);
-  X(sim_id, 1);
-
-  // check dump all returns as expected.
-  _state = wavefunction_type{ 0., 0., 0., 1., 0., 0., 0., 0. };
-  dump(sim_id, "dump-02");
-  Dump(sim_id, check_state);
-
-  // test for a subsystem.
-  _state = wavefunction_type{ 0., 1., 0., 0. };
-  qs[0] = 1;
-  qs[1] = 2;
-  assert(sim_DumpQubits(sim_id, 2, qs, check_state));
-
-  // the dumped state should be consistent based on the qubits given.
-  _state = wavefunction_type{ 0., 0., 1., 0. };
-  qs[0] = 2;
-  qs[1] = 1;
-  assert(sim_DumpQubits(sim_id, 2, qs, check_state));
-
-  // check that when we ask for a different order of all the qubits, 
-  // the corresponding basis state is correct
-  _state = wavefunction_type{ 0., 0., 0., 0., 0., 0., 1., 0. };
-  qs[0] = 2;
-  qs[1] = 1;
-  qs[2] = 0;
-  assert(sim_DumpQubits(sim_id, 3, qs, check_state));
-
-  // however the overall state should not be affected
-  _state = wavefunction_type{ 0., 0., 0., 1., 0., 0., 0., 0. };
-  Dump(sim_id, check_state);
-
-  // entangle two qubits:
-  H(sim_id, 2);
-  qs[0] = 2;
-  MCX(sim_id, 1, qs, 1);
-
-  // check entangle worked:
-  _state = wavefunction_type{ 0., 0., 0., 0.7071, 0., 0.7071, 0., 0. };
-  dump(sim_id, "dump-03");
-  Dump(sim_id, check_state);
-
-  // the subsystem of the two entangled qubits should work:
-  _state = wavefunction_type{ 0., 0.7071, 0.7071, 0. };
-  qs[0] = 1;
-  qs[1] = 2;
-  assert(sim_DumpQubits(sim_id, 2, qs, check_state));
-  // since the qubit is externally entangled, it should return false.
-  qs[0] = 0;
-  qs[1] = 1;
-  assert(sim_DumpQubits(sim_id, 2, qs, no_checks) == false);
-  assert(sim_DumpQubits(sim_id, 1, &qs[1], no_checks) == false);
-
-  // all done:
-  M(sim_id, 0);
-  M(sim_id, 1);
-  M(sim_id, 2);
-
-  release(sim_id, 0);
-  release(sim_id, 1);
-  release(sim_id, 2);
-
-  // check that dump can be called if no more allocated qubits:
-  _state = wavefunction_type{ 1., 0. };
-  dump(sim_id, "dump-end");
-  Dump(sim_id, check_state);
-
-  destroy(sim_id);
-}
-
-
-void test_dump_qubits()
-{
-    auto sim_id = init();
-    constexpr auto num_qubits = 4u;
-    auto qs = std::array<unsigned, num_qubits>{};
-    for (auto i = 0u; i < qs.size(); ++i) {
-        qs[i] = i;
-        allocateQubit(sim_id, i);
+std::vector<std::int32_t> splitNums(const std::string& str, char delim = ',') {
+    std::vector<std::int32_t> nums;
+    size_t start;
+    size_t end = 0;
+    while ((start = str.find_first_not_of(delim, end)) != std::string::npos) {
+        end = str.find(delim, start);
+        nums.push_back(stoi(str.substr(start, end - start)));
     }
-
-    // change state
-    X(sim_id, 1);
-    X(sim_id, 2);
-
-    // check permutations of qubits that should leave the wavefunction invariant
-    _state = wavefunction_type{ 0., 0., 0., 0.,
-                                0., 0., 1., 0.,
-                                0., 0., 0., 0.,
-                                0., 0., 0., 0. };
-    DumpQubits(sim_id, num_qubits, qs.data(), check_state);
-    std::swap(qs[1], qs[2]);
-    DumpQubits(sim_id, num_qubits, qs.data(), check_state);
-    std::swap(qs[0], qs[3]);
-    DumpQubits(sim_id, num_qubits, qs.data(), check_state);
-    std::swap(qs[1], qs[2]);
-    DumpQubits(sim_id, num_qubits, qs.data(), check_state);
-
-    // all done:
-    X(sim_id, 1);
-    X(sim_id, 2);
-
-    for (auto i = 0u; i < qs.size(); ++i)
-        release(sim_id, i);
-    destroy(sim_id);
+    return nums;
 }
 
-*/
+std::vector<std::vector<std::int32_t>> loadTest(char* fName,bool doClusters) {
+    std::vector<std::vector<std::int32_t>> rslt;
+    std::vector<std::int32_t> empty;
+    string line;
+    ifstream file(fName);
+    int phase = 0;
+    if (doClusters) phase = 2;
 
-void test_permute_basis()
-{
-    auto sim_id = init();
+    regex reOrig("^=== Original:");
+    regex reGate("^\\s*(\\d+):\\s+(.+)\\[(.*)\\]");
+    regex reClusters("^=== Clusters .*");
+    regex reCluster("^==== cluster\\[\\s*(\\d+)\\]:.*");
+    smatch sm;
 
-    // oracle for |a>|b> -> |a>|a XOR b> on two-qubit registers |a>, |b>
-    auto permutation = [](auto index) {
-        auto a = index & 3;
-        auto b = (index >> 2) & 3;
-        return a | (a ^ b) << 2;
-    };
-    constexpr auto nqubits = 4u;
-    constexpr auto nstates = 1ul << nqubits;
-    std::size_t table[nstates];
-    for (std::size_t i = 0; i < nstates; ++i) {
-        table[i] = permutation(i);
+    while (getline(file, line)) {
+        if (phase == 99) break;
+        switch (phase) {
+        case 0:
+            if (regex_match(line, sm, reOrig)) phase = 1;
+            break;
+        case 1:
+            if (regex_match(line, sm, reGate)) {
+                auto qs = splitNums(sm[3]);
+                rslt.push_back(qs);
+            }
+            else phase = 99;
+            break;
+        case 2:
+            if (regex_match(line, reClusters)) 
+                phase = 3;
+            break;
+        case 4:
+            if (regex_match(line, sm, reGate)) {
+                auto qs = splitNums(sm[3]);
+                rslt.push_back(qs);
+                break;
+            }
+            else phase = 3;
+        case 3:
+            if (regex_match(line, sm, reCluster)) {
+                rslt.push_back(empty);
+                phase = 4;
+            }
+            break;
+        }
     }
-
-    std::vector<unsigned> qbit_ids(nqubits);
-    for (unsigned i = 0; i < qbit_ids.size(); ++i)
-        qbit_ids[i] = i + 1;
-
-    for (unsigned i = 0; i < nqubits + 1; ++i)
-        allocateQubit(sim_id, i);
-    // Dump(sim_id, "permute-start.txt");
-
-    // create some quantum state
-    H(sim_id, 1);
-    CX(sim_id, 1, 2);
-    Ry(sim_id, 1.1, 3);
-    Ry(sim_id, 2.2, 4);
-
-    // apply permutation - this should be equivalent to CX(sim_id, 0, 2); CX(sim_id, 1, 3);
-    PermuteBasis(sim_id, nqubits, qbit_ids.data(), nstates, table);
-    // Dump(sim_id, "permute-middle.txt");
-
-    // check uncompute with CX instead of permutation
-    CX(sim_id, 2, 4);
-    CX(sim_id, 1, 3);
-    Ry(sim_id, -2.2, 4);
-    Ry(sim_id, -1.1, 3);
-    CX(sim_id, 1, 2);
-    H(sim_id, 1);
-
-    // Dump(sim_id, "permute-end.txt");
-    assert(M(sim_id, 0) == false);
-    assert(M(sim_id, 1) == false);
-    assert(M(sim_id, 2) == false);
-    assert(M(sim_id, 3) == false);
-    assert(M(sim_id, 4) == false);
-
-    for (unsigned i = 0; i < nqubits + 1; ++i)
-        release(sim_id, i);
-    destroy(sim_id);
-}
-
-void test_permute_basis_adjoint()
-{
-    auto sim_id = init();
-
-    // oracle for |a>|b> -> |a>|a XOR b> on two-qubit registers |a>, |b>
-    auto permutation = [](auto index) {
-        auto a = index & 3;
-        auto b = (index >> 2) & 3;
-        return a | (a ^ b) << 2;
-    };
-    constexpr auto nqubits = 4u;
-    constexpr auto nstates = 1ul << nqubits;
-    std::size_t table[nstates];
-    for (std::size_t i = 0; i < nstates; ++i) {
-        table[i] = permutation(i);
-    }
-
-    std::vector<unsigned> qbit_ids(nqubits);
-    for (unsigned i = 0; i < nqubits; ++i)
-        qbit_ids[i] = i + 1;
-
-    for (unsigned i = 0; i < nqubits + 1; ++i)
-        allocateQubit(sim_id, i);
-
-    // create some quantum state
-    H(sim_id, 1);
-    CX(sim_id, 1, 2);
-    Ry(sim_id, 1.1, 3);
-    Ry(sim_id, 2.2, 4);
-
-    // apply permutation - this should be equivalent to CX(sim_id, 0, 2); CX(sim_id, 1, 3);
-    PermuteBasis(sim_id, nqubits, qbit_ids.data(), nstates, table);
-
-    // check uncompute with adjoint=true
-    AdjPermuteBasis(sim_id, nqubits, qbit_ids.data(), nstates, table);
-    Ry(sim_id, -2.2, 4);
-    Ry(sim_id, -1.1, 3);
-    CX(sim_id, 1, 2);
-    H(sim_id, 1);
-
-    assert(M(sim_id, 0) == false);
-    assert(M(sim_id, 1) == false);
-    assert(M(sim_id, 2) == false);
-    assert(M(sim_id, 3) == false);
-    assert(M(sim_id, 4) == false);
-
-    for (unsigned i = 0; i < nqubits + 1; ++i)
-        release(sim_id, i);
-    destroy(sim_id);
+    file.close();
+    return rslt;
 }
 
 int main()
 {
-#if 0 // Original tests
-    std::cerr << "Testing allocate\n";
-    test_allocate();
-    std::cerr << "Testing gates\n";
-    test_gates();
-    std::cerr << "Testing teleport\n";
-    test_teleport();
-    std::cerr << "Testing basis state permutation\n";
-    test_permute_basis();
-    test_permute_basis_adjoint();
-    std::cerr << "Testing dump\n";
-    // test_dump();
-    // test_dump_qubits();
-    return 0;
-#endif
+    auto orig   = loadTest("..\\..\\..\\shor_4.log", false);
+    auto clus   = loadTest("..\\..\\..\\shor_4.log", true);
 
-#if 1 // Simulator timing tests
     int fuseLimits[]    = {0,1,2,5,10,50,100};
     int qCount[]        = {15,26};
     for (int qIdx = 0; qIdx < 2; qIdx++) {                                  // #### 0,2 Number of qubits (15 or 26)
@@ -448,10 +187,10 @@ int main()
             printf("@@@DBG nQs=%d max=%d procs=%d thrds=%d range=%d\n", 
                 nQs, omp_get_max_threads(), omp_get_num_procs(), omp_get_num_threads(),doRange);
             fflush(stdout);
-            for (int fuseSpan = 1; fuseSpan < 8; fuseSpan++) {                  // #### 1,8 Span Size
+            for (int fuseSpan = 6; fuseSpan < 8; fuseSpan++) {                  // #### 1,8 Span Size
                 for (int flIdx = 6; flIdx < 7; flIdx++) {                       // #### 6,7 Span Depth in fuseLimits[]
-                    for (int numThreads = 1; numThreads < 5; numThreads++) {   // #### 1,5 (or 1,17 for big machine)
-                        for (int simTyp = 1; simTyp < 5; simTyp++) {            // #### 1,5 (1=Generic,2=AVX,3=AVX2,4=AVX512)
+                    for (int numThreads = 4; numThreads < 5; numThreads++) {   // #### 1,5 (or 1,17 for big machine)
+                        for (int simTyp = 4; simTyp < 5; simTyp++) {            // #### 1,5 (1=Generic,2=AVX,3=AVX2,4=AVX512)
                             if (simTyp == 4 && (!Microsoft::Quantum::haveAVX512())) continue;
                             if (simTyp == 3 && (!Microsoft::Quantum::haveFMA() || !Microsoft::Quantum::haveAVX2())) continue;
                             if (simTyp == 2 && !Microsoft::Quantum::haveAVX()) continue;
@@ -466,6 +205,7 @@ int main()
                             }
 
                             std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+#if 0
                             for (int i = 0; i < 1000000; i++) {
                                 for (int k = circStart; k < circStop; k++) {
                                     unsigned c = k - 1;
@@ -476,6 +216,30 @@ int main()
                                         for (int j = 0; j < 5; j++)
                                             H(sim_id, k);
                                 }
+#else
+                            for (int i=0; i<1000; i++) {
+                                for (int i = 0; i < orig.size(); i++) {
+                                    auto qs = orig[i];
+                                    switch (qs.size()) {
+                                    case 0: // Need to force a flush (end of cluster)
+                                        break;
+                                    case 1:
+                                        H(sim_id, qs[0]);
+                                        break;
+                                    case 2:
+                                        CX(sim_id, qs[0], qs[1]);
+                                        break;
+                                    case 3:
+                                    {
+                                        uint32_t cs[] = { qs[0], qs[1] };
+                                        MCX(sim_id, 2, cs, qs[2]);
+                                    }
+                                    break;
+                                    default:
+                                        throw(std::invalid_argument("Didn't expect more then 3 wire gates"));
+                                    }
+                                }
+#endif
 
                                 std::chrono::system_clock::time_point curr = std::chrono::system_clock::now();
                                 std::chrono::duration<double> elapsed = curr - start;
@@ -489,7 +253,6 @@ int main()
             }
         }
     }
-#endif
 
 #if 0 // OpenMP test
     double thrd1elapsed = 1.0;
