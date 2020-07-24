d298140802fcd0635f19b520df03d3b6f44867e8
Andres Paz <anpaz@microsoft.com>
2020-07-23 20:48:09 -0700

    native build

--- build/build.ps1
+++ build/build.ps1
@@ -9,7 +9,7 @@ $all_ok = $True
 $nativeBuild = (Join-Path $PSScriptRoot "../src/Simulation/Native/build") 
 if (Test-Path $nativeBuild) {
     Write-Host "##[info]Build Native simulator"
-    cmake --build--config $Env:BUILD_CONFIGURATION
+    cmake --build $nativeBuild --config $Env:BUILD_CONFIGURATION
     if ($LastExitCode -ne 0) {
         Write-Host "##vso[task.logissue type=error;]Failed to build Native simulator."
         $script:all_ok = $False
