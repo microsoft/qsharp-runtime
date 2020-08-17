c0b5f70e9b4f3434717a9d99467f26a56c8f89f4
Stefan J. Wernli <swernli@microsoft.com>
2020-08-17 00:07:55 -0700

    Fixing capi_test use of Pauli basis (#340)

--- src/Simulation/Native/src/simulator/capi_test.cpp
+++ src/Simulation/Native/src/simulator/capi_test.cpp
@@ -23,12 +23,12 @@ void CZ(unsigned sim_id, unsigned c, unsigned q)
 
 void Ry(unsigned sim_id, double phi, unsigned q)
 {
-    R(sim_id,2,phi,q);
+    R(sim_id,3,phi,q);
 }
 
 void CRz(unsigned sim_id, double phi, unsigned c, unsigned q)
 {
-    MCR(sim_id,3,phi,1,&c,q);
+    MCR(sim_id,2,phi,1,&c,q);
 }
 
 void CRx(unsigned sim_id, double phi, unsigned c, unsigned q)
