42e7e3cb1d75e398057fce0730cf37b93e0a2f46
Dave Wecker <wecker@microsoft.com>
2020-08-27 09:59:39 -0700

    Updating files for pull request

--- src/Simulation/Native/src/simulator/factory.cpp
+++ src/Simulation/Native/src/simulator/factory.cpp
@@ -34,47 +34,13 @@ namespace Microsoft
 {
   namespace Quantum
   {
-#ifdef DBWDBG // Defaults for the library
-      int dbgFusedSpan  = -1;
-      int dbgFusedLimit = 99;
-      int dbgNumThreads = 0;
-      int dbgReorder    = 2; // bit0: doReorder bit1: Schedule
-#endif
-
     namespace Simulator
     {
       std::shared_mutex _mutex;
       std::vector<std::shared_ptr<SimulatorInterface>> _psis;
 
-#ifndef DBWDBG
      SimulatorInterface* createSimulator(unsigned maxlocal)
      {
-#else // DBWDBG Override createSimulator with debug parameters
-      SimulatorInterface* createSimulator(unsigned maxlocal,int force=0,int fusedSpan=-1, int fusedLimit=99, int numThreads=0,int reorder=0)
-      {
-          dbgFusedSpan = fusedSpan;
-          dbgFusedLimit = fusedLimit;
-          dbgNumThreads = numThreads;
-          dbgReorder = reorder;
-
-        if (force > 0) {
-            switch (force) {
-            case 1:
-                printf("@@@DBG: Generic\n");
-                return SimulatorGeneric::createSimulator(maxlocal);
-            case 2: 
-                printf("@@@DBG: AVX\n");
-                return SimulatorAVX::createSimulator(maxlocal);
-            case 3: 
-                printf("@@@DBG: AVX2\n");
-                return SimulatorAVX2::createSimulator(maxlocal);
-            case 4:
-                printf("@@@DBG: AVX512\n");
-                return SimulatorAVX512::createSimulator(maxlocal);
-            }
-        }
-#endif // DBWDBG
-
        if (haveAVX512())
         {
             return SimulatorAVX512::createSimulator(maxlocal);
@@ -120,35 +86,6 @@ namespace Microsoft
 
         return static_cast<unsigned>(emptySlot);
       }
-
-#ifdef DBWDBG // Create a debug version of the simulator
-      MICROSOFT_QUANTUM_DECL unsigned createDBG(unsigned maxlocal,int force,int fusedSpan,int fusedLimit,int numThreads,int reorder)
-      {
-          std::lock_guard<std::shared_mutex> lock(_mutex);
-
-          size_t emptySlot = -1;
-          for (auto const& s : _psis)
-          {
-              if (s == NULL)
-              {
-                  emptySlot = &s - &_psis[0];
-                  break;
-              }
-          }
-
-          if (emptySlot == -1)
-          {
-              _psis.push_back(std::shared_ptr<SimulatorInterface>(createSimulator(maxlocal,force,fusedSpan,fusedLimit,numThreads,reorder)));
-              emptySlot = _psis.size() - 1;
-          }
-          else
-          {
-              _psis[emptySlot] = std::shared_ptr<SimulatorInterface>(createSimulator(maxlocal,force,fusedSpan,fusedLimit,numThreads,reorder));
-          }
-
-          return static_cast<unsigned>(emptySlot);
-      }
-#endif
 
       MICROSOFT_QUANTUM_DECL void destroy(unsigned id)
       {
