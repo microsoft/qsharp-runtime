7651e183ef0f1db58265aa2a025379216618becd
Stefan J. Wernli <swernli@microsoft.com>
2020-08-30 13:46:37 -0700

    Show native test output

--- build/test.ps1
+++ build/test.ps1
@@ -9,7 +9,7 @@ if ($Env:ENABLE_NATIVE -ne "false") {
     pushd (Join-Path $PSScriptRoot "../src/Simulation/Native/build")
     cmake --build . --config $Env:BUILD_CONFIGURATION
     cp ../advantage_44_4.log .
-    ctest -C $Env:BUILD_CONFIGURATION
+    ctest -C $Env:BUILD_CONFIGURATION --verbose
     if ($LastExitCode -ne 0) {
         Write-Host "##vso[task.logissue type=error;]Failed to test Native Simulator"
         $script:all_ok = $False
