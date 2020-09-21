ce9ceb4ff42524e4122291f89047a9a6c3dd55e2
Stefan J. Wernli <swernli@microsoft.com>
2020-09-21 10:08:53 -0700

    Fixing typo

--- src/Simulation/Simulators.Type1/Properties/AssemblyInfo.cs
+++ src/Simulation/Simulators.Type1/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Allow the simulator assembly to use our internal methods
+// Allow the test assembly to use our internal methods
 [assembly: InternalsVisibleTo("Tests.Microsoft.Quantum.Simulators.Type1" + SigningConstants.PUBLIC_KEY)]
