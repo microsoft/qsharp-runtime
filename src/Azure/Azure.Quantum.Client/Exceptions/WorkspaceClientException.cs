30f09872603cbba59fdfb14c3edd6269cc8d1461
Ricardo Espinoza <ricardoe@microsoft.com>
2020-09-21 22:38:44 -0700

    Improving the logging of WorkspaceClientException to include more informative data in its message from its inner exception.

--- src/Azure/Azure.Quantum.Client/Exceptions/WorkspaceClientException.cs
+++ src/Azure/Azure.Quantum.Client/Exceptions/WorkspaceClientException.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Microsoft.Rest;
+using Microsoft.Azure.Quantum.Client.Models;
 using System;
 
 namespace Microsoft.Azure.Quantum.Exceptions
@@ -67,9 +69,36 @@ namespace Microsoft.Azure.Quantum.Exceptions
                   $"WorkspaceName: {workspaceName}{Environment.NewLine}" +
                   $"BaseUri: {baseUri}{Environment.NewLine}" +
                   $"JobId: {jobId}{Environment.NewLine}" +
-                  (inner != null ? $"Inner Exception: {inner}" : string.Empty),
+                  FormatInnerException(inner),
                   inner)
         {
+        }
+
+        /// <summary>
+        /// Formats the contents of the inner exception in <see cref="WorkspaceClientException"/> so it can be included in the
+        /// exception message and presented in an informative way.
+        /// </summary>
+        /// <param name="ex">Inner exception that we want to include in the outer exception message.</param>
+        /// <return>
+        /// A string representing the contents of the inner exception.
+        /// </return>
+        private static string FormatInnerException(Exception ex)
+        {
+            string formattedException = string.Empty;
+            if (ex != null)
+            {
+                formattedException += $"Server Error: {ex.Message}{Environment.NewLine}";
+
+                // Handle specific types of exceptions for additional data
+                RestErrorException restErrorException = ex as RestErrorException;
+                if (restErrorException != null)
+                {
+                    formattedException += $"Error code: {restErrorException?.Body?.Code}{Environment.NewLine}" +
+                        $"Server message: {restErrorException?.Body?.Message}{Environment.NewLine}";
+                }
+            }
+
+            return formattedException;
         }
     }
 }
