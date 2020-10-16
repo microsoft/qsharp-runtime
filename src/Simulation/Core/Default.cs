d4d22ab06ba9db198e0ab79a4bca78caca4b63f9
Sarah Marshall <sarah.marshall@microsoft.com>
2020-10-16 15:54:17 -0700

    Add doc comments

--- src/Simulation/Core/Default.cs
+++ src/Simulation/Core/Default.cs
@@ -7,9 +7,15 @@ using System.Linq;
 
 namespace Microsoft.Quantum.Simulation.Core
 {
+    /// <summary>
+    /// Creates default values of Q# types.
+    /// </summary>
     public static class Default
     {
-        private static readonly IReadOnlyDictionary<Type, object> Values = new Dictionary<Type, object>
+        /// <summary>
+        /// A dictionary from basic types to their default values.
+        /// </summary>
+        private static readonly IReadOnlyDictionary<Type, object> BasicValues = new Dictionary<Type, object>
         {
             [typeof(QRange)] = QRange.Empty,
             [typeof(QVoid)] = QVoid.Instance,
@@ -17,6 +23,9 @@ namespace Microsoft.Quantum.Simulation.Core
             [typeof(string)] = ""
         };
 
+        /// <summary>
+        /// A list of all generic tuple types.
+        /// </summary>
         private static readonly IReadOnlyList<Type> Tuples = new List<Type>
         {
             typeof(ValueTuple<>),
@@ -29,29 +38,50 @@ namespace Microsoft.Quantum.Simulation.Core
             typeof(ValueTuple<,,,,,,,>)
         };
 
+        /// <summary>
+        /// Returns the default value of the Q# type. May return null when null is the default value of the type, or if
+        /// the type is not a valid Q# type.
+        /// </summary>
         [return: MaybeNull]
         public static T OfType<T>() => OfType(typeof(T)) is T value ? value : default;
 
+        /// <summary>
+        /// Returns the default value of the Q# type. May return null when null is the default value of the type, or if
+        /// the type is not a valid Q# type.
+        /// </summary>
         private static object? OfType(Type type) => OfAnyType(type).FirstOrDefault(value => !(value is null));
 
+        /// <summary>
+        /// Enumerates the default values of different kinds of types. Yields null if the given type is not the right
+        /// kind, and yields a non-null value if a default value is found.
+        /// </summary>
         private static IEnumerable<object?> OfAnyType(Type type)
         {
-            yield return Values.GetValueOrDefault(type);
+            yield return BasicValues.GetValueOrDefault(type);
             yield return OfArrayType(type);
             yield return OfTupleType(type);
             yield return OfUserDefinedType(type);
         }
 
+        /// <summary>
+        /// If the given type is a Q# array type, returns the default array of that type, or null otherwise.
+        /// </summary>
         private static object? OfArrayType(Type type) =>
             type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IQArray<>)
                 ? Activator.CreateInstance(typeof(QArray<>).MakeGenericType(type.GenericTypeArguments))
                 : null;
 
+        /// <summary>
+        /// If the given type is a Q# tuple type, returns the default tuple of that type, or null otherwise.
+        /// </summary>
         private static object? OfTupleType(Type type) =>
             type.IsGenericType && Tuples.Contains(type.GetGenericTypeDefinition())
                 ? Activator.CreateInstance(type, type.GenericTypeArguments.Select(OfType).ToArray())
                 : null;
 
+        /// <summary>
+        /// If the given type is a Q# user-defined type, returns the default value of that type, or null otherwise.
+        /// </summary>
         private static object? OfUserDefinedType(Type type) =>
             !(type.BaseType is null)
             && type.BaseType.IsGenericType
