b0dec095ae5ebcdffb4412b0d1c5744b3cd8c55c
Irina Yatsenko <irinayat@microsoft.com>
2020-10-23 08:37:35 -0700

    Standartize formatting across native simulator files

--- src/Simulation/Native/src/simulator/simulatoravx512.cpp
+++ src/Simulation/Native/src/simulator/simulatoravx512.cpp
@@ -7,10 +7,9 @@
 
 #include "simulator/simulator.hpp"
 
-
 namespace sim = Microsoft::Quantum::SimulatorAVX512;
 
 MICROSOFT_QUANTUM_DECL Microsoft::Quantum::Simulator::SimulatorInterface* sim::createSimulator(unsigned maxlocal)
 {
-  return new sim::SimulatorType(maxlocal);
+    return new sim::SimulatorType(maxlocal);
 }
