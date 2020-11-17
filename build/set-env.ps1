2e825d7a2b9516bee9dd7713456b3406fe2b06ee
Irina Yatsenko <irinayat@microsoft.com>
2020-11-17 12:42:22 -0800

    Fix the check for build agent to enable QIR Runtime CI

--- build/set-env.ps1
+++ build/set-env.ps1
@@ -11,7 +11,7 @@ If ($Env:BUILD_VERBOSITY -eq $null) { $Env:BUILD_VERBOSITY ="m" }
 If ($Env:ASSEMBLY_VERSION -eq $null) { $Env:ASSEMBLY_VERSION ="$Env:BUILD_BUILDNUMBER" }
 If ($Env:NUGET_VERSION -eq $null) { $Env:NUGET_VERSION ="$Env:ASSEMBLY_VERSION-alpha" }
 
-If (-not $Env:BUILD_AGENT -eq $null) {
+If ($Env:BUILD_AGENT -ne $null) {
     If ($Env:ENABLE_QIRRUNTIME -eq $null) { $Env:ENABLE_QIRRUNTIME ="true" }
     If (($Env:ENABLE_NATIVE -ne "false") -and ($Env:NATIVE_SIMULATOR -eq $null) ) {
         $Env:NATIVE_SIMULATOR = (Join-Path $PSScriptRoot "..\src\Simulation\Native\build\$Env:BUILD_CONFIGURATION")
