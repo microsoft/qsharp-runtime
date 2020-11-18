edd649c50596729f930f7fa1784b8371a3f55f88
Irina Yatsenko <36858951+irinayat-MS@users.noreply.github.com>
2020-11-17 16:12:50 -0800

    Identify CI builds by presence of the BUILD_REASON environment variable

--- build/set-env.ps1
+++ build/set-env.ps1
@@ -11,7 +11,8 @@ If ($Env:BUILD_VERBOSITY -eq $null) { $Env:BUILD_VERBOSITY ="m" }
 If ($Env:ASSEMBLY_VERSION -eq $null) { $Env:ASSEMBLY_VERSION ="$Env:BUILD_BUILDNUMBER" }
 If ($Env:NUGET_VERSION -eq $null) { $Env:NUGET_VERSION ="$Env:ASSEMBLY_VERSION-alpha" }
 
-If ($Env:BUILD_AGENT -ne $null) {
+# By default don't build QIR Runtime locally. CI builds are identified by presence of the BUILD_REASON variable.
+If ($Env:BUILD_REASON -ne $null) {
     If ($Env:ENABLE_QIRRUNTIME -eq $null) { $Env:ENABLE_QIRRUNTIME ="true" }
     If (($Env:ENABLE_NATIVE -ne "false") -and ($Env:NATIVE_SIMULATOR -eq $null) ) {
         $Env:NATIVE_SIMULATOR = (Join-Path $PSScriptRoot "..\src\Simulation\Native\build\$Env:BUILD_CONFIGURATION")
