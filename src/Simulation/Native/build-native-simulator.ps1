2dd246ba825820498ac649081cbb55e04c4896a8
Irina Yatsenko <irinayat@microsoft.com>
2020-11-18 21:58:45 -0800

    Build native simulator to match the rest of the runtime

--- src/Simulation/Native/build-native-simulator.ps1
+++ src/Simulation/Native/build-native-simulator.ps1
@@ -4,13 +4,19 @@
 Write-Host "##[info]Build Native simulator"
 
 $compiler = ""
-if (Test-Path Env:AGENT_OS) {
-    if ($Env:AGENT_OS.StartsWith("Darwin")) {
-        $compiler = "-DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7"
-        Write-Host "The system is identified as MacOS: $IsMacOS"
-    }
+if (($IsMacOS) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Darwin"))))
+{
+        $compiler = "-DCMAKE_C_COMPILER= gcc-7 -DCMAKE_CXX_COMPILER= g++-7 "
+}
+elseif (($IsLinux) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Lin"))))
+{
+    $compiler = "-DCMAKE_C_COMPILER= gcc -DCMAKE_CXX_COMPILER= g++"
+}
+elseif (($IsWindows) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Win"))))
+{
+    # use default, which should be MSVC
 } else {
-    Write-Host "Native Simulator should be built on MacOS using gcc7."
+    Write-Host "##vso[task.logissue type=error;]Failed to identify the OS. Will use default CXX compiler"
 }
 
 $nativeBuild = (Join-Path $PSScriptRoot "build\$Env:BUILD_CONFIGURATION")
@@ -19,7 +25,8 @@ if (-not (Test-Path $nativeBuild)) {
 }
 Push-Location $nativeBuild
 
-cmake $compiler -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_BUILD_TYPE= $BuildConfiguration ../..
+# We never build debug build of native simulator? (-DCMAKE_BUILD_TYPE= $BuildConfiguration)
+cmake $compiler -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_BUILD_TYPE= $Env:BUILD_CONFIGURATION ../..
 cmake --build .
 
 Pop-Location
