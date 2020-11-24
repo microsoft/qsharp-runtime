b221fb60a00f9801528242bd1565905384fb0e12
Irina Yatsenko <irinayat@microsoft.com>
2020-11-23 18:25:12 -0800

    Add back the temporary workaround for e2e pipeline to succeed with new build scripts

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,13 +3,19 @@
 
 $ErrorActionPreference = 'Stop'
 
-If ($Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION -eq $null) {
-    $Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION = "Release"
-}
+$Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION = "Release"
 
 Push-Location (Join-Path $PSScriptRoot "build")
     .\prerequisites.ps1
 Pop-Location
+
+# Temporary hack until switch qdk build pipeline to use the new build scripts (as a result it will build the native 
+# simulator twice, but the second build should be mostly noop)
+if ((Test-Path Env:AGENT_OS) -and ($Env:CI -ne "true") -and ($Env:ENABLE_NATIVE -ne "false")) {
+    Push-Location (Join-Path $PSScriptRoot "src/Simulation/Native")
+        .\build-native-simulator.ps1
+    Pop-Location
+}
 
 # When setting up local dev environment, build Release version of the native simulator and Debug version of the managed
 # part of the runtime.
