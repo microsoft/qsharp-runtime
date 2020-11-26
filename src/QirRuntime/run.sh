dc102ff66664239f598100a53cf3f15d0e85dc3f
Guen Prawiroatmodjo <guenp@microsoft.com>
2020-11-25 18:01:12 -0800

    compile and run executable

---
+++ src/QirRuntime/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 build.py
+build/Linux/Debug/bin/qir-exe 0.001 -0.001 0.001 1
--- src/QirRuntime/test/QIR-exe/main.cpp
+++ src/QirRuntime/test/QIR-exe/main.cpp
@@ -18,15 +18,15 @@ extern "C" void DebugLogPtr(char* value)
 
 // This VQE sample is taken from https://github.com/msr-quarc/StandaloneVQE
 // When executed in Q# it returns -1.0 with single decimal precision (the correct energy value is close to -1.3)
-extern "C" double Microsoft__Quantum__Samples__Chemistry__SimpleVQE__GetEnergyHydrogenVQE__body(); // NOLINT
-
+// Declaration of entrypoint, provided in QIR file
+extern "C" double Microsoft__Quantum__Samples__Chemistry__SimpleVQE__GetEnergyHydrogenVQE__body(double theta1, double theta2, double theta3, int64_t nSamples); // NOLINT
 using namespace std;
 using namespace Microsoft::Quantum;
 int main(int argc, char* argv[]) noexcept
 {
     try
     {
-        if (argc != 2)
+        if (argc != 5)
         {
             // argv[0] should contain the program name
             std::cout << "usage: " << argv[0] << " <number of iterations>\n";
@@ -36,13 +36,13 @@ int main(int argc, char* argv[]) noexcept
         unique_ptr<ISimulator> qapi = CreateFullstateSimulator();
         SetSimulatorForQIR(qapi.get());
 
-        const long iters = atol(argv[1]);
-
-        for (long i = 0; i < iters; i++)
-        {
-            const double ret = Microsoft__Quantum__Samples__Chemistry__SimpleVQE__GetEnergyHydrogenVQE__body();
-            std::cout << ret << std::endl;
-        }
+        const double theta1 = atof(argv[1]);
+        const double theta2 = atof(argv[2]);
+        const double theta3 = atof(argv[3]);
+        const int64_t iters = atol(argv[4]);
+        
+        const double ret = Microsoft__Quantum__Samples__Chemistry__SimpleVQE__GetEnergyHydrogenVQE__body(theta1, theta2, theta3, iters);
+        std::cout << ret << std::endl;
     }
     catch (const std::exception& e)
     {
