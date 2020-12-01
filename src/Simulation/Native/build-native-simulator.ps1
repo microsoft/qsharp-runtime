40bbe69db9b1f7cb5fbaf779f8c28ba7c2bb283c
Irina Yatsenko <irinayat@microsoft.com>
2020-11-30 23:34:42 -0800

    Diagnostics show that the build-native-simulator.ps1 always produces Debug on Linux. Try not using env var in the command line

--- src/Simulation/Native/build-native-simulator.ps1
+++ src/Simulation/Native/build-native-simulator.ps1
@@ -13,30 +13,31 @@ Push-Location $nativeBuild
 if (($IsWindows) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Win"))))
 {
     Write-Host "On Windows build native simulator using the default C/C++ compiler (should be MSVC)"
-    Write-Host 'cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_BUILD_TYPE=$Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION ..'
     cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_BUILD_TYPE=$Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION ..
 } elseif (($IsLinux) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Lin")))) {
     Write-Host "On Linux build native simulator using gcc (needed for OpenMP)"
-    Write-Host 'cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=$Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION ..'
-    cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=$Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION ..
+    if ($Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION -eq "Release") {
+        cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release ..
+    } else {
+        cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug ..
+    }
 }
 elseif (($IsMacOS) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Darwin")))) {
     Write-Host "On MacOS build native simulator using gcc (needed for OpenMP)"
     # `gcc`on Darwin seems to be a shim that redirects to AppleClang, to get real gcc, must point to the specific
     # version of gcc we've installed.
-    Write-Host 'cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7 -DCMAKE_BUILD_TYPE=$Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION ..'
     cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7 -DCMAKE_BUILD_TYPE=$Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION ..
 }
 else {
     Write-Host "Failed to recognize the platform, will attempt to build with default compiler"
-    Write-Host 'cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_BUILD_TYPE=$Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION ..'
     cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_BUILD_TYPE=$Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION ..
 }
-Write-Host 'cmake --build . --config $Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION --target install'
 cmake --build . --config $Env:NATIVE_SIMULATOR_BUILD_CONFIGURATION --target install
 
-Write-Host "size of libMicrosoft.Quantum.Simulator.Runtime.so file (if exists, expected for Release build to be around 976,200 bytes)"
-Write-Host ((Get-Item libMicrosoft.Quantum.Simulator.Runtime.so).length)
+if (($IsLinux) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Lin")))) {
+    Write-Host "size of libMicrosoft.Quantum.Simulator.Runtime.so file (if exists, expected for Release build to be around 976,200 bytes)"
+    Write-Host ((Get-Item libMicrosoft.Quantum.Simulator.Runtime.so).length)
+}
 Pop-Location
 
 
