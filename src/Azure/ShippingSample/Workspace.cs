1dcc7a2653cce091f94989716d34de7ec8b3b8f4
Roman Snytsar <rosnytsa@microsoft.com>
2020-12-02 07:54:05 -0800

    C# QIO API

---
+++ src/Azure/ShippingSample/Workspace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Azure.Quantum;
+
+namespace ShippingSample
+{
+    partial class Program
+    {
+        static Workspace CreateWorkspace()
+        {
+            // Instantiate Workspace object which allows you to connect to the Workspace you"ve previously deployed in Azure. 
+            // Be sure to fill in the settings below which can be retrieved by running "az quantum workspace show" in the terminal.
+            // Copy the settings for your workspace below
+            var workspace = new Workspace(
+                subscriptionId: "", // add your subscription_id
+                resourceGroupName: "", // add your resource_group
+                workspaceName: "", // add your workspace name
+                baseUri: new Uri("https://westus.quantum.azure.com/"));
+
+            return workspace;
+        }
+    }
+}
