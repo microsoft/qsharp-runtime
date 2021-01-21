f271d5a16a3fecc3ab63a2bd3c18aacc8e8738c1
Sarah Marshall <sarah.marshall@microsoft.com>
2021-01-21 11:40:14 -0800

    Add space between -D and name in CMake command

--- src/Simulation/Native/build-native-simulator.ps1
+++ src/Simulation/Native/build-native-simulator.ps1
@@ -10,28 +10,28 @@ if (-not (Test-Path $nativeBuild)) {
 }
 Push-Location $nativeBuild
 
-# There should be no space after -DCMAKE_BUILD_TYPE= but if we provide the environment variable inline, without
-# the space it doesn't seem to get substituted... With invalid -DCMAKE_BUILD_TYPE argument cmake silently produces
+# There should be no space after -D CMAKE_BUILD_TYPE= but if we provide the environment variable inline, without
+# the space it doesn't seem to get substituted... With invalid -D CMAKE_BUILD_TYPE argument cmake silently produces
 # a DEBUG build.
 if (($IsWindows) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Win"))))
 {
     Write-Host "On Windows build native simulator using the default C/C++ compiler (should be MSVC)"
-    cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" ..
+    cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" ..
 }
 elseif (($IsLinux) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Lin"))))
 {
     Write-Host "On Linux build native simulator using gcc (needed for OpenMP)"
-    cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" ..
+    cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" ..
 }
 elseif (($IsMacOS) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Darwin"))))
 {
     Write-Host "On MacOS build native simulator using gcc (needed for OpenMP)"
     # `gcc`on Darwin seems to be a shim that redirects to AppleClang, to get real gcc, must point to the specific
     # version of gcc we've installed.
-    cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7 -DCMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" ..
+    cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_C_COMPILER=gcc-7 -D CMAKE_CXX_COMPILER=g++-7 -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" ..
 }
 else {
-    cmake -DBUILD_SHARED_LIBS:BOOL="1" -DCMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" ..
+    cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" ..
 }
 cmake --build . --config "$Env:BUILD_CONFIGURATION" --target install
 
@@ -41,4 +41,3 @@ Pop-Location
 if ($LastExitCode -ne 0) {
     Write-Host "##vso[task.logissue type=error;]Failed to build Native simulator."
 }
-
