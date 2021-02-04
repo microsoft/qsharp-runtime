7206a17201fd620573ab9eb303792fded1a037fe
Stefan J. Wernli <swernli@microsoft.com>
2021-02-04 11:02:42 -0800

    Updates for CR feedback

--- src/Simulation/Simulators/QuantumSimulator/ApplyUncontrolledSWAP.cs
+++ src/Simulation/Simulators/QuantumSimulator/ApplyUncontrolledSWAP.cs
@@ -11,6 +11,10 @@ namespace Microsoft.Quantum.Simulation.Simulators
     {
         public virtual void ApplyUncontrolledSWAP__Body(Qubit qubit1, Qubit qubit2)
         {
+            // Issue #44 (https://github.com/microsoft/qsharp-runtime/issues/44)
+            // If/when the simulator provides access to the accelerated SWAP functionality,
+            // this can be replaced with a call to that instead of MCX.
+
             this.CheckQubits(new QArray<Qubit>(new Qubit[]{ qubit1, qubit2 }));
 
             MCX(this.Id, 1, new uint[]{(uint)qubit1.Id}, (uint)qubit2.Id);
