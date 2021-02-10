a30f93a846137b0b9e198e83f3d4f7fa09283ad8
Christopher Granade <cgranade@cgranade.com>
2021-02-09 19:35:08 -0800

    Add rust to builds.

--- src/QirRuntime/prerequisites.ps1
+++ src/QirRuntime/prerequisites.ps1
@@ -5,6 +5,7 @@ if ($Env:ENABLE_QIRRUNTIME -eq "true") {
     if (($IsWindows) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Win")))) {
         choco install llvm
         choco install ninja
+        choco install rustup
     } else {
         #brew install llvm  # this seems to mess up native simulator build, probably because of STD libs
                             # llvm should be already available on later Linux/Darwin systems
