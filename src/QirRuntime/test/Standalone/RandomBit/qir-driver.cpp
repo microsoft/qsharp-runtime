e58f672371e169845db5a1db288492fd8f012c31
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2021-02-15 13:40:04 -0800

    Added simulator and Q# operation output redirection through command line options.

--- src/QirRuntime/test/Standalone/RandomBit/qir-driver.cpp
+++ src/QirRuntime/test/Standalone/RandomBit/qir-driver.cpp
@@ -19,10 +19,68 @@ extern "C" bool Microsoft__Quantum__Testing__QIR__RandomBit__body(); // NOLINT
 
 int main(int argc, char *argv[])
 {
-    std::cout << "QIR Driver" << std::endl;
-    SetupQirToRunOnFullStateSimulator();
-    
-    bool bit = Microsoft__Quantum__Testing__QIR__RandomBit__body();
-    std::cout << bit << std::endl;
+    try{
+        CLI::App app("Random Bit");
+
+        // Add the --simulator-output and --operation-output options.
+        // N.B. These options should be present in all standalone drivers.
+        std::string simulatorOutputFile;
+        CLI::Option *simulatorOutputFileOpt = app.add_option(
+            "-s,--simulator-output",
+            simulatorOutputFile,
+            "File to write the output of the simulator to");
+
+        std::string operationOutputFile;
+        CLI::Option *operationOutputFileOpt = app.add_option(
+            "-o,--operation-output",
+            operationOutputFile,
+            "File to write the output of the Q# operation to");
+
+        CLI11_PARSE(app, argc, argv);
+
+        // Redirect the simulator output from std::cout if the --simulator-output option is present.
+        std::ostream* simulatorOutputStream = &std::cout;
+        std::filebuf simulatorOutputFileBuffer;
+        if (!simulatorOutputFileOpt->empty()){
+            std::cout << "Simulator Output File: " << simulatorOutputFile << std::endl;
+            simulatorOutputFileBuffer.open(simulatorOutputFile, std::ios::out);
+            std::ostream simulatorOutputFileStream(&simulatorOutputFileBuffer);
+            // TODO: Call into the QIR runtime API to redirect the output of the simulator.
+            simulatorOutputStream = &simulatorOutputFileStream;
+        }
+
+        // TODO: This is for testing purposes only.
+        (*simulatorOutputStream) << "SIMULATOR OUTPUT:\nSample Output\n";
+
+        // Redirect the Q# opertion output from std::cout if the --operation-output option is present.
+        std::ostream* operationOutputStream = &std::cout;
+        std::filebuf operationOutputFileBuffer;
+        if (!operationOutputFileOpt->empty()){
+            std::cout << "Operation Output File: " << operationOutputFile << std::endl;
+            operationOutputFileBuffer.open(operationOutputFile, std::ios::out);
+            std::ostream operationOutputFileStream(&operationOutputFileBuffer);
+            operationOutputStream = &operationOutputFileStream;
+        }
+
+        // Start simulation.
+        SetupQirToRunOnFullStateSimulator();
+        bool bit = Microsoft__Quantum__Testing__QIR__RandomBit__body();
+        simulatorOutputStream->flush();
+        (*operationOutputStream) << "OPERATION OUTPUT:\n";
+        (*operationOutputStream) << (bit ? "true" : "false");
+        operationOutputStream->flush();
+
+        // Close opened file buffers;
+        if (!simulatorOutputFileOpt->empty()){
+            simulatorOutputFileBuffer.close();
+        }
+
+        if (!operationOutputFileOpt->empty()){
+            operationOutputFileBuffer.close();
+        }
+    } catch(...) {
+        return 1;
+    }
+
     return 0;
 }
