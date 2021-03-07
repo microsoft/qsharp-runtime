079f87e085a73fe83ed8ddeee34dd16dd08a325a
Stefan J. Wernli <swernli@microsoft.com>
2021-03-07 05:54:10 +0000

    Fix cmake existence check for generated obj

--- src/QirRuntime/cmake/qir_cmake_include.cmake
+++ src/QirRuntime/cmake/qir_cmake_include.cmake
@@ -56,13 +56,13 @@ macro(compile_from_qir source_file target)
 endmacro(compile_from_qir)
 
 macro(target_source_from_qir_obj target_name source_file)
-    target_sources(${target_name} PUBLIC 
-        "${CMAKE_CURRENT_BINARY_DIR}/${source_file}.obj"
-    )
     SET_SOURCE_FILES_PROPERTIES(
         "${source_file}.obj"
         PROPERTIES
         EXTERNAL_OBJECT true
         GENERATED true
+    )
+    target_sources(${target_name} PUBLIC 
+        "${CMAKE_CURRENT_BINARY_DIR}/${source_file}.obj"
     )
 endmacro()
