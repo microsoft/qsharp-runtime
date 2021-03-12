8b4a430956f7f0cfa731eb93458e4df93ee03d56
Stefan J. Wernli <swernli@microsoft.com>
2021-03-11 21:48:14 -0800

    Remove llvm-ar depenency (#556)
    We shouldn't need llvm-ar.exe anymore, so we can remove that cached external component.
    
    Fixes #554

--- src/QirRuntime/build-qir-runtime.ps1
+++ src/QirRuntime/build-qir-runtime.ps1
@@ -38,8 +38,6 @@ elseif (($IsWindows) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWit
     $env:CC = "clang.exe"
     $env:CXX = "clang++.exe"
     $env:RC = "clang++.exe"
-    $llvmExtras = Join-Path $PSScriptRoot externals LLVM
-    $env:PATH += ";$llvmExtras"
 
     if (!(Get-Command clang -ErrorAction SilentlyContinue) -and (choco find --idonly -l llvm) -contains "llvm") {
         # LLVM was installed by Chocolatey, so add the install location to the path.
