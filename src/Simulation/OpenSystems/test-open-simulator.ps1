e806e1ae200e7218981c93c4f533ea420a24da02
Christopher Granade <cgranade@cgranade.com>
2021-03-16 13:53:29 -0700

    Start enabling performance reporting for opensim.

--- src/Simulation/OpenSystems/test-open-simulator.ps1
+++ src/Simulation/OpenSystems/test-open-simulator.ps1
@@ -1,3 +1,28 @@
+& (Join-Path $PSScriptRoot ".." ".." ".." "build" "set-env.ps1");
+
 Push-Location (Join-Path $PSScriptRoot runtime)
-    cargo test
+    # If running in CI, use cargo2junit to expose unit tests to the
+    # PublishTestResults task.
+    if ("$Env:TF_BUILD" -ne "") {
+        cargo install cargo2junit
+        cargo test -- -Z unstable-options --format json `
+            | cargo2junit `
+            <# We use this name to match the *_results.xml pattern that is used
+               to find test results in steps-wrap-up.yml. #> `
+            | Out-File -FilePath opensim_results.xml -Encoding utf8NoBOM
+    } else {
+        # Outside of CI, show human-readable output.
+        cargo test
+    }
+
+    # Run performance benchmarks as well.
+    cargo bench
+
+    # This step isn't required, but we use it to upload run summaries.
+    $reportPath = (Join-Path "target" "criterion");
+    $perfDest = (Join-Path $Env:DROPS_DIR "perf" "opensim");
+    if (Get-Item -ErrorAction SilentlyContinue $reportPath) {
+        New-Item -Type Directory -Force -Path $perfDest;
+        Copy-Item -Recurse -Path $reportPath -Destination $perfDest;
+    }
 Pop-Location
