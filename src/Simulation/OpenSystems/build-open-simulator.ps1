9b1707236db67a5bc00e35f15878661e79106151
Christopher Granade <cgranade@cgranade.com>
2021-03-17 23:21:26 -0700

    Clear diskspace and build microbenchmarks in CI.

--- src/Simulation/OpenSystems/build-open-simulator.ps1
+++ src/Simulation/OpenSystems/build-open-simulator.ps1
@@ -3,4 +3,9 @@
 Push-Location (Join-Path $PSScriptRoot runtime)
     $releaseFlag = "$Env:BUILD_CONFIGURATION" -eq "Release" ? @("--release") : @();
     cargo build @releaseFlag;
+
+    # Free disk space by cleaning up target/${config}/deps.
+    # Note that this takes longer, but saves ~1 GB of space, which is
+    # exceptionally helpful in CI builds.
+    Remove-Item -Recurse (Join-Path . target ("$Env:BUILD_CONFIGURATION" -eq "Release" ? "release" : "debug") "deps");
 Pop-Location
