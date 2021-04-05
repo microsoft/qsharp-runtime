c7f55a53e5f682cfffe2d937216f8e53a491d34f
César Zaragoza Cortés <cesarzc@microsoft.com>
2021-04-05 10:57:44 -0700

    Setup controller project (#597)
    * Create controller project.
    * Add script to test the QIR controller.
    * Test QIR controller as part of the build.
    * Renamed test controller script.
    * Update test-case file.

---
+++ src/Qir/Controller/Controller.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.IO;
+using System.Text;
+
+namespace Microsoft.Quantum.Qir
+{
+    internal static class Controller
+    {
+        internal static void Execute(
+            FileInfo input,
+            FileInfo output,
+            FileInfo error)
+        {
+            var outputFileStream = output.Exists ? output.OpenWrite() : output.Create();
+            outputFileStream.Write(new UTF8Encoding().GetBytes("output"));
+            outputFileStream.Flush();
+            outputFileStream.Close();
+            var errorFileStream = error.Exists ? error.OpenWrite() : error.Create();
+            errorFileStream.Write(new UTF8Encoding().GetBytes("error"));
+            errorFileStream.Flush();
+            errorFileStream.Close();
+        }
+    }
+}
---
+++ src/Qir/Controller/Program.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+
+namespace Microsoft.Quantum.Qir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rootCommand = new RootCommand(
+                description: "Builds and runs QIR executable.");
+
+            // Create and add options to the root command.
+            var inputOption = new Option<FileInfo>(
+                aliases: new string[] { "--input"})
+            {
+                Description = "Path to the file that contains the input.",
+                IsRequired = true
+            };
+            
+            rootCommand.AddOption(inputOption);
+            var outputOption = new Option<FileInfo>(
+                aliases: new string[] { "--output"})
+            {
+                Description = "Path to the file to which the output will be written.",
+                IsRequired = true
+            };
+
+            rootCommand.AddOption(outputOption);
+            var errorOption = new Option<FileInfo>(
+                aliases: new string[] { "--error",})
+            {
+                Description = "Path to the file to which errors will be logged.",
+                IsRequired = true
+            };
+
+            rootCommand.AddOption(errorOption);
+
+            // Bind to a handler and invoke.
+            rootCommand.Handler = CommandHandler.Create<FileInfo, FileInfo, FileInfo>((input, output, error) => Controller.Execute(input, output, error));
+            rootCommand.Invoke(args);
+        }
+    }
+}
