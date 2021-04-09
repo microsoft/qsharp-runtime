ef1a2f8e5091cfde94a245d2a8b84a08274dfa70
Alexander Chocron <alchocro@microsoft.com>
2021-04-09 12:05:48 -0700

    Add comment

--- src/Qir/Controller/Executable/ClangClient.cs
+++ src/Qir/Controller/Executable/ClangClient.cs
@@ -20,6 +20,8 @@ namespace Microsoft.Quantum.Qir.Executable
         public async Task CreateExecutableAsync(string[] inputFiles, string[] libraries, string libraryPath, string includePath, string outputPath)
         {
             var inputsArg = string.Join(' ', inputFiles);
+
+            // string.Join does not automatically prepend the delimiter, so it is included again in the string here.
             var librariesArg = $"{LinkFlag} {string.Join(LinkFlag, libraries)}";
             var arguments = $"{inputsArg} -I {includePath} -L {libraryPath} {librariesArg} -o {outputPath}";
             logger.LogInfo($"Invoking clang with the following arguments: {arguments}");
