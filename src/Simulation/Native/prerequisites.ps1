2f9a02e9c5e7c9b25991f8006bcfed99d5f43d10
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-04-12 13:49:06 -0700

    Add note on temporary 'brew update' workaround

--- src/Simulation/Native/prerequisites.ps1
+++ src/Simulation/Native/prerequisites.ps1
@@ -4,6 +4,9 @@
 if (($IsMacOS) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Darwin")))) {
     # building with gcc-9 succeeds but some of the unit tests fail
     Write-Host "Install gcc-7 as pre-req for building native simulator on MacOS"
+    # temporary workaround for Bintray sunset
+    # remove this after Homebrew is updated to 3.1.1 on MacOS image, see:
+    # https://github.com/actions/virtual-environments/blob/main/images/macos/macos-10.15-Readme.md
     brew update
     brew install gcc@7
 } else {
