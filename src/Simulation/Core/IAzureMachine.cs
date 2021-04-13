22d1b23d83bb3e5005cbf5db55808870ba85051a
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2021-04-12 20:04:22 -0700

    Create IQirMachine interface.

---
+++ src/Simulation/Core/IAzureMachine.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.Quantum.Simulation.Core;
+
+namespace Microsoft.Quantum.Runtime
+{
+    public interface IAzureMachine
+    {
+        /// <summary>
+        /// Function that configures a job object before submission.
+        /// </summary>
+        public delegate void ConfigureJob(object job);
+
+        /// <summary>
+        /// Gets the ID of the quantum machine provider.
+        /// </summary>
+        string ProviderId { get; }
+
+        /// <summary>
+        /// Gets the name of the target quantum machine.
+        /// A provider may expose multiple targets that can be used to execute programs.
+        /// Users may select which target they would like to be used for execution.
+        /// </summary>
+        string Target { get; }
+    }
+}
---
+++ src/Simulation/Core/IQirMachine.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.Quantum.Simulation.Core;
+
+namespace Microsoft.Quantum.Runtime
+{
+    public interface IQirMachine : IAzureMachine
+    {
+        /// <summary>
+        /// Submits a job to execute a Q# program.
+        /// Does not wait for execution to be completed.
+        /// </summary>
+        /// <param name="info">Information about the Q# program.</param>
+        /// <param name="input">Input for the Q# program.</param>
+        /// <param name="qir">Input for the Q# program.</param>
+        /// <typeparam name="TInput">Type of input the quantum program receives.</typeparam>
+        /// <typeparam name="TOutput">Type of output the quantum program returns.</typeparam>
+        /// <returns>An object that implements the IQuantumMachineJob interface through which data about the job can be obtained.</returns>
+        Task<IQuantumMachineJob> SubmitAsync<TInput, TOutput>(
+            EntryPointInfo<TInput, TOutput> info,
+            TInput input,
+            byte[] qir) => SubmitAsync(info, input, qir, null);
+
+        /// <summary>
+        /// Submits a job to execute a Q# program.
+        /// Does not wait for execution to be completed.
+        /// </summary>
+        /// <param name="info">Information about the Q# program.</param>
+        /// <param name="input">Input for the Q# program.</param>
+        /// <param name="qir">Input for the Q# program.</param>
+        /// <param name="configureJobCallback">Function that configures a job object before submission.</param>
+        /// <typeparam name="TInput">Type of input the quantum program receives.</typeparam>
+        /// <typeparam name="TOutput">Type of output the quantum program returns.</typeparam>
+        /// <returns>An object that implements the IQuantumMachineJob interface through which data about the job can be obtained.</returns>
+        Task<IQuantumMachineJob> SubmitAsync<TInput, TOutput>(
+            EntryPointInfo<TInput, TOutput> info,
+            TInput input,
+            byte[] qir,
+            ConfigureJob configureJobCallback);
+    }
+}
