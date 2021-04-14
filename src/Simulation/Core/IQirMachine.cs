4ad0265f44e3c258cde6db39cd197418d3d48610
Sarah Marshall <sarah.marshall@microsoft.com>
2021-04-13 18:12:29 -0700

    Add Argument class

--- src/Simulation/Core/IQirMachine.cs
+++ src/Simulation/Core/IQirMachine.cs
@@ -1,28 +1,99 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Quantum.Simulation.Core;
+using Core = Microsoft.Quantum.Simulation.Core;
 
 namespace Microsoft.Quantum.Runtime
 {
     public interface IQirMachine : IAzureMachine
     {
         /// <summary>
-        /// Submits a job to execute a QIR program. Does not wait for execution to be completed.
+        /// Submits a job to execute a QIR program without waiting for execution to complete.
         /// </summary>
-        /// <param name="info">The entry point information.</param>
-        /// <param name="input">The input to the entry point.</param>
         /// <param name="qir">The QIR program as a byte stream.</param>
-        /// <typeparam name="TInput">Type of input the QIR program receives.</typeparam>
-        /// <typeparam name="TOutput">Type of output the QIR program returns.</typeparam>
-        /// <returns>
-        /// An object that implements the IQuantumMachineJob interface through which data about the job can be obtained.
-        /// </returns>
-        Task<IQuantumMachineJob> SubmitAsync<TInput, TOutput>(
-            EntryPointInfo<TInput, TOutput> info,
-            TInput input,
-            Stream qir);
+        /// <param name="entryPoint">The fully-qualified name of the entry point to execute.</param>
+        /// <param name="arguments">The arguments to the entry point.</param>
+        /// <returns>The submitted job.</returns>
+        Task<IQuantumMachineJob> SubmitAsync(Stream qir, string entryPoint, IReadOnlyList<Argument> arguments);
+    }
+
+    public class Argument
+    {
+        public string Name { get; }
+
+        public ArgumentValue Value { get; }
+
+        public Argument(string name, ArgumentValue value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+    }
+
+    public abstract class ArgumentValue
+    {
+        private ArgumentValue()
+        {
+        }
+
+        public class Bool : ArgumentValue
+        {
+            public bool Value { get; }
+
+            public Bool(bool value) => this.Value = value;
+        }
+
+        public class Int : ArgumentValue
+        {
+            public long Value { get; }
+
+            public Int(long value) => this.Value = value;
+        }
+
+        public class Double : ArgumentValue
+        {
+            public double Value { get; }
+
+            public Double(double value) => this.Value = value;
+        }
+
+        public class Pauli : ArgumentValue
+        {
+            public Core.Pauli Value { get; }
+
+            public Pauli(Core.Pauli value) => this.Value = value;
+        }
+
+        public class Range : ArgumentValue
+        {
+            public QRange Value { get; }
+
+            public Range(QRange value) => this.Value = value;
+        }
+
+        public class Result : ArgumentValue
+        {
+            public Core.Result Value { get; }
+
+            public Result(Core.Result value) => this.Value = value;
+        }
+
+        public class String : ArgumentValue
+        {
+            public string Value { get; }
+
+            public String(string value) => this.Value = value;
+        }
+
+        public class Array : ArgumentValue
+        {
+            public QArray<ArgumentValue> Values { get; }
+
+            public Array(QArray<ArgumentValue> values) => this.Values = values;
+        }
     }
 }
