1e4129dc84bac733e7819d8be2383763e90c5350
Sarah Marshall <sarah.marshall@microsoft.com>
2021-04-13 18:25:33 -0700

    Tuple assignment

--- src/Simulation/Core/IQirMachine.cs
+++ src/Simulation/Core/IQirMachine.cs
@@ -27,11 +27,7 @@ namespace Microsoft.Quantum.Runtime
 
         public ArgumentValue Value { get; }
 
-        public Argument(string name, ArgumentValue value)
-        {
-            this.Name = name;
-            this.Value = value;
-        }
+        public Argument(string name, ArgumentValue value) => (this.Name, this.Value) = (name, value);
     }
 
     public abstract class ArgumentValue
@@ -43,48 +39,56 @@ namespace Microsoft.Quantum.Runtime
         public class Bool : ArgumentValue
         {
             public bool Value { get; }
+
             public Bool(bool value) => this.Value = value;
         }
 
         public class Int : ArgumentValue
         {
             public long Value { get; }
+
             public Int(long value) => this.Value = value;
         }
 
         public class Double : ArgumentValue
         {
             public double Value { get; }
+
             public Double(double value) => this.Value = value;
         }
 
         public class Pauli : ArgumentValue
         {
             public Core.Pauli Value { get; }
+
             public Pauli(Core.Pauli value) => this.Value = value;
         }
 
         public class Range : ArgumentValue
         {
             public QRange Value { get; }
+
             public Range(QRange value) => this.Value = value;
         }
 
         public class Result : ArgumentValue
         {
             public Core.Result Value { get; }
+
             public Result(Core.Result value) => this.Value = value;
         }
 
         public class String : ArgumentValue
         {
             public string Value { get; }
+
             public String(string value) => this.Value = value;
         }
 
         public class Array : ArgumentValue
         {
             public IQArray<ArgumentValue> Values { get; }
+
             public Array(IQArray<ArgumentValue> values) => this.Values = values;
         }
     }
