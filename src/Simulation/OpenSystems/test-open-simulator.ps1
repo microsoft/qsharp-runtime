5e2ec23478df103b842200dcd67fc242ca25765f
Christopher Granade <cgranade@cgranade.com>
2021-04-14 08:21:22 -0700

    Clean up extra cargo files after testing opensim.

--- src/Simulation/OpenSystems/test-open-simulator.ps1
+++ src/Simulation/OpenSystems/test-open-simulator.ps1
@@ -25,4 +25,14 @@ Push-Location (Join-Path $PSScriptRoot runtime)
         New-Item -Type Directory -Force -Path $perfDest;
         Copy-Item -Recurse -Path $reportPath -Destination $perfDest;
     }
+
+    # Free disk space by cleaning up target/${config}/deps.
+    # Note that this takes longer, but saves ~1 GB of space, which is
+    # exceptionally helpful in CI builds.
+    @("release", "debug") | ForEach-Object {
+        $config = $_;
+        @("deps", "build", "incremental") | ForEach-Object {
+            Remove-Item -Recurse (Join-Path . target $config $_) -ErrorAction Continue -Verbose;
+        }
+    }
 Pop-Location
