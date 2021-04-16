d2937ee03448b3b672197d474c314435e389ffdd
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2021-04-16 13:23:34 -0700

    Included build methods.

--- src/Qir/Execution/Tools/QirExecutable.cs
+++ src/Qir/Execution/Tools/QirExecutable.cs
@@ -1,10 +1,31 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using Microsoft.Quantum.QsCompiler.BondSchemas.EntryPoint;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace Microsoft.Quantum.Qir.Tools
 {
     public abstract class QirExecutable
     {
+        private readonly EntryPointOperation EntryPointOperation;
+        private readonly byte[] QirBytecode;
 
+        public QirExecutable(EntryPointOperation entryPoint, byte[] qirBytecode)
+        {
+            this.EntryPointOperation = entryPoint;
+            this.QirBytecode = qirBytecode;
+        }
+
+        public void Build(FileInfo executable, DirectoryInfo libraryDirectory, DirectoryInfo includeDirectory)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected abstract void GenerateDriver(Stream driver);
+
+        protected abstract IList<string> GetLinkLibraries();
     }
 }
--- src/Qir/Execution/Tools/QirFullStateExecutable.cs
+++ src/Qir/Execution/Tools/QirFullStateExecutable.cs
@@ -1,9 +1,28 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using Microsoft.Quantum.QsCompiler.BondSchemas.EntryPoint;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace Microsoft.Quantum.Qir.Tools
 {
     public class QirFullStateExecutable : QirExecutable
     {
+        public QirFullStateExecutable(EntryPointOperation entryPoint, byte[] qirBytecode) :
+            base(entryPoint, qirBytecode)
+        {
+        }
+
+        protected override void GenerateDriver(Stream driver)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IList<string> GetLinkLibraries()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
