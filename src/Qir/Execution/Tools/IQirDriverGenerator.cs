fde3f5c82cb3cac1a889a686fefdc64bac70a700
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2021-04-19 22:29:20 -0700

    Created IQirDriverGenerator interface.

---
+++ src/Qir/Execution/Tools/IQirDriverGenerator.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Quantum.QsCompiler.BondSchemas.EntryPoint;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Microsoft.Quantum.Qir.Tools
+{
+    public interface IQirDriverGenerator
+    {
+        public Task GenerateAsync(EntryPointOperation entryPoint, Stream stream);
+
+        public IList<string> LinkLibraries { get; }
+
+        public string SourceType { get; }
+    }
+}
