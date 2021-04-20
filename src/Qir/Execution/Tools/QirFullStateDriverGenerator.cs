9906c6b7f11df604019eb70837e175f6504d0b10
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2021-04-19 22:35:18 -0700

    Created QirFullStateDriverGenerator class.

---
+++ src/Qir/Execution/Tools/QirFullStateDriverGenerator.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Quantum.QsCompiler;
+using Microsoft.Quantum.QsCompiler.BondSchemas.EntryPoint;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Quantum.Qir.Tools
+{
+    public class QirFullStateDriverGenerator : IQirDriverGenerator
+    {
+        public async Task GenerateAsync(EntryPointOperation entryPoint, Stream stream) =>
+            await Task.Run(() => QirDriverGeneration.GenerateQirDriverCpp(entryPoint, stream));
+
+        public IList<string> LinkLibraries => Enumerable.Empty<string>().ToList();
+
+        public string SourceType => "cpp";
+    }
+}
