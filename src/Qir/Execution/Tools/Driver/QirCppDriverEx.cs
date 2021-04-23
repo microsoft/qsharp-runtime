344c36e09748563e960bc81c6757c002cee20879
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2021-04-23 14:20:55 -0700

    Create base for moving QIR driver generator.

---
+++ src/Qir/Execution/Tools/Driver/QirCppDriverEx.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Quantum.Qir.Tools.Driver
+{
+    public partial class QirCppDriver
+    {
+    }
+}
---
+++ src/Qir/Execution/Tools/Driver/QirCppDriverGenerator.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using Microsoft.Quantum.QsCompiler.BondSchemas.EntryPoint;
+
+namespace Microsoft.Quantum.Qir.Tools.Driver
+{
+    public class QirCppDriverGenerator
+    {
+        private readonly IQirSimulatorInitializer SimulatorInitalizer;
+
+        public QirCppDriverGenerator(IQirSimulatorInitializer simulatorInitializer)
+        {
+            SimulatorInitalizer = simulatorInitializer;
+        }
+
+        public void GenerateDriver(EntryPointOperation entryPointOperation, Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
