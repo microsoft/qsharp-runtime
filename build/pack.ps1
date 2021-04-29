fa094eda8612c80b45074045287f03d5068344a4
Andres Paz <anpaz@microsoft.com>
2021-04-28 23:47:42 -0700

    ForcePrerelease

--- build/pack.ps1
+++ build/pack.ps1
@@ -101,7 +101,7 @@ Pack-Dotnet '../src/Simulation/Type3Core/Microsoft.Quantum.Type3.Core.csproj'
 Pack-One '../src/Simulation/Simulators/Microsoft.Quantum.Simulators.nuspec'
 Pack-One '../src/Quantum.Development.Kit/Microsoft.Quantum.Development.Kit.nuspec'
 Pack-One '../src/Xunit/Microsoft.Quantum.Xunit.csproj'
-Pack-One '../src/Qir/Runtime/Microsoft.Quantum.Qir.Runtime.nuspec' -ForcePrelease
+Pack-One '../src/Qir/Runtime/Microsoft.Quantum.Qir.Runtime.nuspec' -ForcePrerelease
 
 if (-not $all_ok) {
     throw "At least one project failed to pack. Check the logs."
