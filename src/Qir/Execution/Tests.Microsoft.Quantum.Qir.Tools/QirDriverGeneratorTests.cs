b522ff4fdd6e877383049bdf05f381c5c536fdc9
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2021-04-30 20:09:42 -0700

    Added test cases.

--- src/Qir/Execution/Tests.Microsoft.Quantum.Qir.Tools/QirDriverGeneratorTests.cs
+++ src/Qir/Execution/Tests.Microsoft.Quantum.Qir.Tools/QirDriverGeneratorTests.cs
@@ -32,11 +32,112 @@ namespace Tests.Microsoft.Quantum.Qir.Tools
                     }
                 },
                 {
+                    "UseBoolArrayArg",
+                    new EntryPointOperation
+                    {
+                        Name = "UseBoolArrayArg",
+                        Parameters = new List<Parameter>{new Parameter{ Name = "BoolArrayArg", Type = DataType.ArrayType, ArrayType = DataType.BoolType}}
+                    }
+                },
+                {
+                    "UseDoubleArg",
+                    new EntryPointOperation
+                    {
+                        Name = "UseDoublArg",
+                        Parameters = new List<Parameter>{new Parameter{ Name = "DoubleArg", Type = DataType.DoubleType}}
+                    }
+                },
+                {
+                    "UseDoubleArrayArg",
+                    new EntryPointOperation
+                    {
+                        Name = "UseDoubleArrayArg",
+                        Parameters = new List<Parameter>{new Parameter{ Name = "DoubleArrayArg", Type = DataType.ArrayType, ArrayType = DataType.DoubleType}}
+                    }
+                },
+                {
+                    "UseIntegerArg",
+                    new EntryPointOperation
+                    {
+                        Name = "UseIntegerArg",
+                        Parameters = new List<Parameter>{new Parameter{ Name = "IntegerArg", Type = DataType.IntegerType}}
+                    }
+                },
+                {
+                    "UseIntegerArrayArg",
+                    new EntryPointOperation
+                    {
+                        Name = "UseIntegerArrayArg",
+                        Parameters = new List<Parameter>{new Parameter{ Name = "IntegerArrayArg", Type = DataType.ArrayType, ArrayType = DataType.IntegerType}}
+                    }
+                },
+                {
+                    "UsePauliArg",
+                    new EntryPointOperation
+                    {
+                        Name = "UsePauliArg",
+                        Parameters = new List<Parameter>{new Parameter{ Name = "PauliArg", Type = DataType.PauliType}}
+                    }
+                },
+                {
+                    "UsePauliArrayArg",
+                    new EntryPointOperation
+                    {
+                        Name = "UsePauliArrayArg",
+                        Parameters = new List<Parameter>{new Parameter{ Name = "PauliArrayArg", Type = DataType.ArrayType, ArrayType = DataType.PauliType}}
+                    }
+                },
+                {
+                    "UseRangeArg",
+                    new EntryPointOperation
+                    {
+                        Name = "UseRangeArg",
+                        Parameters = new List<Parameter>{new Parameter{ Name = "RangeArg", Type = DataType.RangeType}}
+                    }
+                },
+                {
+                    "UseRangeArrayArg",
+                    new EntryPointOperation
+                    {
+                        Name = "UseRangeArrayArg",
+                        Parameters = new List<Parameter>{new Parameter{ Name = "RangeArrayArg", Type = DataType.ArrayType, ArrayType = DataType.RangeType}}
+                    }
+                },
+                {
                     "UseResultArg",
                     new EntryPointOperation
                     {
                         Name = "UseResultArg",
                         Parameters = new List<Parameter>{new Parameter{ Name = "ResultArg", Type = DataType.ResultType}}
+                    }
+                },
+                {
+                    "UseResultArrayArg",
+                    new EntryPointOperation
+                    {
+                        Name = "UseResultArrayArg",
+                        Parameters = new List<Parameter>{new Parameter{ Name = "ResultArrayArg", Type = DataType.ArrayType, ArrayType = DataType.ResultType}}
+                    }
+                },
+                {
+                    "UseStringArg",
+                    new EntryPointOperation
+                    {
+                        Name = "UseStringArg",
+                        Parameters = new List<Parameter>{new Parameter{ Name = "StringArg", Type = DataType.StringType}}
+                    }
+                },
+                {
+                    "UseMiscArgs",
+                    new EntryPointOperation
+                    {
+                        Name = "UseMiscArgs",
+                        Parameters = new List<Parameter>{
+                            new Parameter{ Name = "BoolArg", Type = DataType.BoolType},
+                            new Parameter{ Name = "IntegerArrayArg", Position = 1, Type = DataType.ArrayType, ArrayType = DataType.IntegerType},
+                            new Parameter{ Name = "RangeArg", Position = 2, Type = DataType.RangeType},
+                            new Parameter{ Name = "StringArg", Position = 3, Type = DataType.StringType}
+                        }
                     }
                 }
             };
@@ -44,7 +145,19 @@ namespace Tests.Microsoft.Quantum.Qir.Tools
         [Theory]
         [InlineData("UseNoArgs")]
         [InlineData("UseBoolArg")]
+        [InlineData("UseBoolArrayArg")]
+        [InlineData("UseDoubleArg")]
+        [InlineData("UseDoubleArrayArg")]
+        [InlineData("UseIntegerArg")]
+        [InlineData("UseIntegerArrayArg")]
+        [InlineData("UsePauliArg")]
+        [InlineData("UsePauliArrayArg")]
+        [InlineData("UseRangeArg")]
+        [InlineData("UseRangeArrayArg")]
         [InlineData("UseResultArg")]
+        [InlineData("UseResultArrayArg")]
+        [InlineData("UseStringArg")]
+        [InlineData("UseMiscArgs")]
         public void GenerateFullStateSimulatorDriver(string testCase)
         {
             var entryPointOperation = TestCases[testCase];
