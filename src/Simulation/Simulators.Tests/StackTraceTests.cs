dd841869d125c2e02a6a772fea5553fd9561d23e
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-05-11 15:54:45 -0700

    Minor comment fix.

--- src/Simulation/Simulators.Tests/StackTraceTests.cs
+++ src/Simulation/Simulators.Tests/StackTraceTests.cs
@@ -54,7 +54,7 @@ namespace Microsoft.Quantum.Simulation.Simulators.Tests
                 Assert.Equal(OperationFunctor.Body, stackFrames[1].Callable.Variant);
 
                 // TODO: The following change -
-                //   https://github.com/microsoft/qsharp-runtime/pull/650/files#diff-80e206c7d2a40d77626cd9584ef57d062d3ee7ba01fdc3cc9ca88f7f017dff96 -
+                //   https://github.com/microsoft/qsharp-runtime/pull/675/files#diff-80e206c7d2a40d77626cd9584ef57d062d3ee7ba01fdc3cc9ca88f7f017dff96 (in Assert.qs) -
                 //   (`AssertMeasurement()` body calls the `AssertMeasurementProbability(bases, qubits, result, 1.0, msg, 1e-10)`)
                 //   causes the assert below to fail. The issue in the generator of the stack frames is suspected.
                 //   Deeper analysis is needed.
