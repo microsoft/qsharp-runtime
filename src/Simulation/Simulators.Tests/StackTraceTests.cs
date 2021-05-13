0060c4e43059cf31dbeeef41f1b777d03b7df309
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-05-12 20:11:22 -0700

    Fixed the stack test

--- src/Simulation/Simulators.Tests/StackTraceTests.cs
+++ src/Simulation/Simulators.Tests/StackTraceTests.cs
@@ -47,18 +47,13 @@ namespace Microsoft.Quantum.Simulation.Simulators.Tests
                 // The following assumes that Assert is on Q# stack.
                 Assert.Equal(2, stackFrames!.Length);
 
-                Assert.Equal("Microsoft.Quantum.Diagnostics.AssertMeasurement", stackFrames[0].Callable.FullName);
+                Assert.Equal("Microsoft.Quantum.Diagnostics.AssertMeasurementProbability", stackFrames[0].Callable.FullName);
                 Assert.Equal(namespacePrefix + "AllocateQubit2", stackFrames[1].Callable.FullName);
 
                 Assert.Equal(OperationFunctor.Body, stackFrames[0].Callable.Variant);
                 Assert.Equal(OperationFunctor.Body, stackFrames[1].Callable.Variant);
 
-                // TODO: The following change -
-                //   https://github.com/microsoft/qsharp-runtime/pull/675/files#diff-80e206c7d2a40d77626cd9584ef57d062d3ee7ba01fdc3cc9ca88f7f017dff96 (in Assert.qs) -
-                //   (`AssertMeasurement()` body calls the `AssertMeasurementProbability(bases, qubits, result, 1.0, msg, 1e-10)`)
-                //   causes the assert below to fail. The issue in the generator of the stack frames is suspected.
-                //   Deeper analysis is needed.
-                //Assert.Equal(94, stackFrames[1].FailedLineNumber);
+                Assert.Equal(94, stackFrames[1].FailedLineNumber);
             }
             finally
             {
