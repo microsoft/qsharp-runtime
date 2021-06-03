335d9a02aa3d566fa1d506e46b1f209cf6f26489
Christopher Granade <cgranade@cgranade.com>
2021-06-02 22:07:10 -0700

    Fix build paths.

--- src/Simulation/qdk_sim_rs/build-qdk-sim-rs.ps1
+++ src/Simulation/qdk_sim_rs/build-qdk-sim-rs.ps1
@@ -1,25 +1,16 @@
 & (Join-Path $PSScriptRoot ".." ".." ".." "build" "set-env.ps1");
 
-$DropPath = Join-Path $Env:DROP_NATIVE "qdk_sim_rs";
-
 Push-Location $PSScriptRoot
     $releaseFlag = "$Env:BUILD_CONFIGURATION" -eq "Release" ? @("--release") : @();
 
     # Enable control flow guard (see https://github.com/microsoft/qsharp-runtime/pull/647)
-    # for interoperating Rust and C. This feature is stable (hence -C), but
-    # only provides complete safety guarantees when the standard library is
-    # also rebuilt with the same rustc flags.
+    # for interoperating Rust and C.
     # NB: CFG is only supported on Windows, but the Rust flag is supported on
     #     all platforms; it's ignored on platforms without CFG functionality.
     $Env:RUSTFLAGS = "-C control-flow-guard";
-    # To support rebuilding the Rust standard library with CFG enabled, we need
-    # the Rust source code to be installed with rustup.
-    rustup +nightly component add rust-src;
 
     # Actually run the build.
-    # TODO: enable "-Z build-std" to fully support CFG.
-    Write-Host "##[info]Building qdk_sim_rs into $DropPath...";
-    cargo +nightly build -Z unstable-options @releaseFlag --out-dir $DropPath;
+    cargo build @releaseFlag;
 
     # Make sure docs are complete.
     $Env:RUSTDOCFLAGS = "--html-in-header $(Resolve-Path docs-includes/header.html) " + `
