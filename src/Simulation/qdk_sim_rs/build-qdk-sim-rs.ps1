ce0fde5a162c8406df3ea56b1922a8014493f5b7
Christopher Granade <cgranade@cgranade.com>
2021-06-07 18:10:53 -0700

    Move format/linting checks to build from test.

--- src/Simulation/qdk_sim_rs/build-qdk-sim-rs.ps1
+++ src/Simulation/qdk_sim_rs/build-qdk-sim-rs.ps1
@@ -1,7 +1,22 @@
 & (Join-Path $PSScriptRoot ".." ".." ".." "build" "set-env.ps1");
 $IsCI = "$Env:TF_BUILD" -ne "" -or "$Env:CI" -eq "true";
 
-Push-Location $PSScriptRoot
+Push-Location $PSScriptRoot    
+    # Start with the quick check first and make sure that Rust sources
+    # meet formatting and style guide rules.
+    cargo fmt -- --check
+    $script:allOk = $script:allOk -and $LASTEXITCODE -eq 0;
+
+    # Check linting rules defined by clippy, a linting tool provided with the
+    # Rust toolchain. Please see https://github.com/rust-lang/rust-clippy
+    # and https://rust-lang.github.io/rust-clippy/master/index.html
+    # for more information.
+    # If there's a false positive, that check should be explicitly disabled
+    # at the point where the false positive occurs with an explanation as to
+    # why it's OK.
+    cargo clippy -- -D warnings
+    $script:allOk = $script:allOk -and $LASTEXITCODE -eq 0;
+
     $releaseFlag = "$Env:BUILD_CONFIGURATION" -eq "Release" ? @("--release") : @();
 
     # Enable control flow guard (see https://github.com/microsoft/qsharp-runtime/pull/647)
