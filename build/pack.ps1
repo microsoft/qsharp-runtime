b2560439cfb20d1561ca604f92c9843dce7ef672
Scott Carda <sccarda@microsoft.com>
2021-06-10 13:15:45 -0700

    Added tool to pack

--- build/pack.ps1
+++ build/pack.ps1
@@ -99,6 +99,7 @@ Pack-Dotnet '../src/Simulation/Type1Core/Microsoft.Quantum.Type1.Core.csproj'
 Pack-Dotnet '../src/Simulation/Type2Core/Microsoft.Quantum.Type2.Core.csproj'
 Pack-Dotnet '../src/Simulation/Type3Core/Microsoft.Quantum.Type3.Core.csproj'
 Pack-Dotnet '../src/Qir/Execution/Tools/Microsoft.Quantum.Qir.Tools.csproj' -ForcePrerelease
+Pack-Dotnet '../src/Qir/Execution/QirCommandLineTool/QirCommandLineTool.csproj' -ForcePrerelease
 Pack-One '../src/Simulation/Simulators/Microsoft.Quantum.Simulators.nuspec'
 Pack-One '../src/Quantum.Development.Kit/Microsoft.Quantum.Development.Kit.nuspec'
 Pack-One '../src/Xunit/Microsoft.Quantum.Xunit.csproj'
