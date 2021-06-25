c7a0020001a45ccd063f6898e04e753c8e1f3639
Scott Carda <sccarda@microsoft.com>
2021-06-25 14:13:05 -0700

    add todo

--- src/Qir/Execution/Tools/Executable/QirFullStateExecutable.cs
+++ src/Qir/Execution/Tools/Executable/QirFullStateExecutable.cs
@@ -34,7 +34,7 @@ namespace Microsoft.Quantum.Qir.Tools.Executable
                   new QirFullStateDriverGenerator(),
                   logger)
         {
-            var thisModulePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var thisModulePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); //ToDo
             if (string.IsNullOrWhiteSpace(thisModulePath))
             {
                 throw new InvalidOperationException("Could not get a path for the current assembly location.");
