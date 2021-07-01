21ade66e382200d025a3f2545f641c1360ab8565
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-07-01 16:43:55 -0700

    Excluded Win from UBSan (linker error in Win)

--- src/Qir/qir-utils.ps1
+++ src/Qir/qir-utils.ps1
@@ -99,28 +99,51 @@ function Build-CMakeProject {
 
     # Sanitizers (https://clang.llvm.org/docs/UsersManual.html#controlling-code-generation):
 
-    $env:CFLAGS   += " "
-    $env:CXXFLAGS += " "
+    $sanitizeFlags = "" 
+    if (-not ($IsWindows))
+    {
+        # Undefined Behavior Sanitizer (https://clang.llvm.org/docs/UndefinedBehaviorSanitizer.html)
+        # WSL:
+        #   unsigned-integer-overflow   Causes error reports in "qir-static-tests" in random number generator standard library (the test still succeeds):
+        #       /usr/bin/../lib/gcc/x86_64-linux-gnu/9/../../../../include/c++/9/bits/random.h:552:58: runtime error: unsigned integer overflow: 0 - 1 cannot be represented in type 'unsigned long'
+        #   Likely -fsanitize=implicit-integer-sign-change causes error reports in "qir-static-tests" in random number generator standard library (the test still succeeds):
+        #       /usr/bin/../lib/gcc/x86_64-linux-gnu/9/../../../../include/c++/9/bits/uniform_int_dist.h:284:17: runtime error: implicit conversion from type \
+        #           'std::uniform_int_distribution<long>::result_type' (aka 'long') of value -9223372036854775808 (64-bit, signed) to type 'unsigned long' changed the value \
+        #           to 9223372036854775808 (64-bit, unsigned)
+        # Win:
+        #   [19/35] cmd.exe /C "cd . && C:\PROGRA~1\LLVM12\bin\CLANG_~1.EXE -fuse-ld=lld-link -nostartfiles -nostdlib \
+        #       -Werror -Weverything -Wno-c++98-compat-pedantic -Wno-old-style-cast -Wno-covered-switch-default -Wno-c99-extensions \
+        #       -Wno-padded -Wno-weak-vtables -Wno-global-constructors -Wno-exit-time-destructors -Wno-extra-semi-stmt \
+        #       -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=unsigned-integer-overflow -fsanitize=implicit-conversion \
+        #       -fsanitize=local-bounds -fsanitize=nullability -fno-omit-frame-pointer -fno-optimize-sibling-calls \
+        #       -O2 -g -DNDEBUG -Xclang -gcodeview -D_DLL -D_MT -Xclang --dependent-lib=msvcrt  -Xlinker /guard:cf -shared \
+        #       -o lib\QIR\Microsoft.Quantum.Qir.Runtime.dll  -Xlinker /implib:lib\QIR\Microsoft.Quantum.Qir.Runtime.lib \
+        #       -Xlinker /pdb:lib\QIR\Microsoft.Quantum.Qir.Runtime.pdb -Xlinker /version:0.0 lib/QIR/bridge-rt.obj \
+        #       lib/QIR/CMakeFiles/qir-rt-support-obj.dir/QirRange.cpp.obj lib/QIR/CMakeFiles/qir-rt-support-obj.dir/OutputStream.cpp.obj \
+        #       lib/QIR/CMakeFiles/qir-rt-support-obj.dir/Output.cpp.obj lib/QIR/CMakeFiles/qir-rt-support-obj.dir/allocationsTracker.cpp.obj \
+        #       lib/QIR/CMakeFiles/qir-rt-support-obj.dir/arrays.cpp.obj lib/QIR/CMakeFiles/qir-rt-support-obj.dir/callables.cpp.obj \
+        #       lib/QIR/CMakeFiles/qir-rt-support-obj.dir/context.cpp.obj lib/QIR/CMakeFiles/qir-rt-support-obj.dir/delegated.cpp.obj \
+        #       lib/QIR/CMakeFiles/qir-rt-support-obj.dir/strings.cpp.obj lib/QIR/CMakeFiles/qir-rt-support-obj.dir/utils.cpp.obj \
+        #       lib/QIR/CMakeFiles/qir-rt-support-obj.dir/QubitManager.cpp.obj  -lkernel32 -luser32 -lgdi32 -lwinspool -lshell32 -lole32 \
+        #       -loleaut32 -luuid -lcomdlg32 -ladvapi32 -loldnames && cd ."
+        #   FAILED: lib/QIR/Microsoft.Quantum.Qir.Runtime.dll lib/QIR/Microsoft.Quantum.Qir.Runtime.lib
+        #   lld-link: error: /failifmismatch: mismatch detected for 'RuntimeLibrary':
+        #   >>> lib/QIR/CMakeFiles/qir-rt-support-obj.dir/QubitManager.cpp.obj has value MD_DynamicRelease
+        #   >>> clang_rt.ubsan_standalone_cxx-x86_64.lib(ubsan_type_hash_win.cc.obj) has value MT_StaticRelease
+        #   clang++: error: linker command failed with exit code 1 (use -v to see invocation)
+        $sanitizeFlags += " -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=unsigned-integer-overflow -fsanitize=implicit-conversion -fsanitize=local-bounds -fsanitize=nullability"
+        # TODO: 
+        #     Consider "Silencing Unsigned Integer Overflow" (https://clang.llvm.org/docs/UndefinedBehaviorSanitizer.html#silencing-unsigned-integer-overflow)
+        #         for "qir-static-tests".
+        #     Consider extracting into a separate .cpp or binary and compile with no `-fsanitize=unsigned-integer-overflow`. All others compile with `-fsanitize=unsigned-integer-overflow`.
+        #         Issue: `unsigned-integer-overflow` seems to be a part of some other check that is a part of `-fsanitize=undefined`. See https://github.com/google/sanitizers/issues/1421.
+        #             How to enable `-fsanitize=undefined` except `unsigned-integer-overflow`? Issue filed https://github.com/google/sanitizers/issues/1422.
+        #     Consider calming down `-fsanitize=implicit-integer-sign-change` for "qir-static-tests".
+        #     For Win consider extra build configuration linking all libs staticly, and enable `-fsanitize=undefined`, run the staticly linked tests.
 
-    # Undefined Behavior Sanitizer (https://clang.llvm.org/docs/UndefinedBehaviorSanitizer.html)
-    # WSL:
-    #   unsigned-integer-overflow   Causes error reports in "qir-static-tests" in random number generator standard library (the test still succeeds):
-    #       /usr/bin/../lib/gcc/x86_64-linux-gnu/9/../../../../include/c++/9/bits/random.h:552:58: runtime error: unsigned integer overflow: 0 - 1 cannot be represented in type 'unsigned long'
-    #   Likely -fsanitize=implicit-integer-sign-change causes error reports in "qir-static-tests" in random number generator standard library (the test still succeeds):
-    #       /usr/bin/../lib/gcc/x86_64-linux-gnu/9/../../../../include/c++/9/bits/uniform_int_dist.h:284:17: runtime error: implicit conversion from type \
-    #           'std::uniform_int_distribution<long>::result_type' (aka 'long') of value -9223372036854775808 (64-bit, signed) to type 'unsigned long' changed the value \
-    #           to 9223372036854775808 (64-bit, unsigned)
-    $sanitizeFlags = "-fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=unsigned-integer-overflow -fsanitize=implicit-conversion -fsanitize=local-bounds -fsanitize=nullability"
-    # TODO: 
-    #     Consider "Silencing Unsigned Integer Overflow" (https://clang.llvm.org/docs/UndefinedBehaviorSanitizer.html#silencing-unsigned-integer-overflow)
-    #         for "qir-static-tests".
-    #     Consider extracting into a separate .cpp or binary and compile with no `-fsanitize=unsigned-integer-overflow`. All others compile with `-fsanitize=unsigned-integer-overflow`.
-    #         Issue: `unsigned-integer-overflow` seems to be a part of some other check that is a part of `-fsanitize=undefined`. See https://github.com/google/sanitizers/issues/1421.
-    #             How to enable `-fsanitize=undefined` except `unsigned-integer-overflow`? Issue filed https://github.com/google/sanitizers/issues/1422.
-    #     Consider calming down `-fsanitize=implicit-integer-sign-change` for "qir-static-tests".
-
-    $sanitizeFlags += " -fno-omit-frame-pointer"            # https://clang.llvm.org/docs/UndefinedBehaviorSanitizer.html
-    $sanitizeFlags += " -fno-optimize-sibling-calls"        # https://clang.llvm.org/docs/AddressSanitizer.html
+        $sanitizeFlags += " -fno-omit-frame-pointer"            # https://clang.llvm.org/docs/UndefinedBehaviorSanitizer.html
+        $sanitizeFlags += " -fno-optimize-sibling-calls"        # https://clang.llvm.org/docs/AddressSanitizer.html
+    }
 
     $env:CFLAGS   += $sanitizeFlags
     $env:CXXFLAGS += $sanitizeFlags
