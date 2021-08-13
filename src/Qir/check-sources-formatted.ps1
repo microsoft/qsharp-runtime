609f302acf62b4bf6236c04b2b712278c34893b8
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-08-12 18:53:51 -0700

    Inv

--- src/Qir/check-sources-formatted.ps1
+++ src/Qir/check-sources-formatted.ps1
@@ -8,21 +8,34 @@ param (
 
 $tmpFile = "format.log"
 
+#Write-Host "1"
+#"$DirPath/*.cpp","$DirPath/*.c","$DirPath/*.h","$DirPath/*.hpp" | get-childitem -Recurse `
+#    | ?{$_.fullname -notlike "*\Externals\*"} | ?{$_.fullname -notlike "*\drops\*"} | ?{$_.fullname -notlike "*\bin\*"} `
+#    | %{clang-format -n -style=file $_.fullname}
+#
+#Write-Host "2"
+#"$DirPath/*.cpp","$DirPath/*.c","$DirPath/*.h","$DirPath/*.hpp" | get-childitem -Recurse `
+#    | ?{$_.fullname -notlike "*\Externals\*"} | ?{$_.fullname -notlike "*\drops\*"} | ?{$_.fullname -notlike "*\bin\*"} `
+#    | %{clang-format -n -style=file $_.fullname} 2>format.log
+
+#Write-Host "3"
+#$filesRequireFormatting = get-content $tmpFile | ?{$_ -like "*: warning:*"} `
+#                            | %{[string]::join(":",($_.split("warning:")[0].split(":") | select -SkipLast 3))} `
+#                            | sort | unique
+#Remove-Item $tmpFile
+
 Write-Host "1"
-"$DirPath/*.cpp","$DirPath/*.c","$DirPath/*.h","$DirPath/*.hpp" | get-childitem -Recurse `
-    | ?{$_.fullname -notlike "*\Externals\*"} | ?{$_.fullname -notlike "*\drops\*"} | ?{$_.fullname -notlike "*\bin\*"} `
-    | %{clang-format -n -style=file $_.fullname}
+&{
+   Write-Warning "warning"
+   #Write-Error "error"
+   Write-Output "output"
+} *>$tmpFile
 
 Write-Host "2"
-"$DirPath/*.cpp","$DirPath/*.c","$DirPath/*.h","$DirPath/*.hpp" | get-childitem -Recurse `
-    | ?{$_.fullname -notlike "*\Externals\*"} | ?{$_.fullname -notlike "*\drops\*"} | ?{$_.fullname -notlike "*\bin\*"} `
-    | %{clang-format -n -style=file $_.fullname} 2>format.log
+type $tmpFile
 
 Write-Host "3"
-$filesRequireFormatting = get-content $tmpFile | ?{$_ -like "*: warning:*"} `
-                            | %{[string]::join(":",($_.split("warning:")[0].split(":") | select -SkipLast 3))} `
-                            | sort | unique
-#Remove-Item $tmpFile
+throw "Formatting check failed for QIR Runtime sources"
 
 if (! ("$filesRequireFormatting" -eq ""))
 {
