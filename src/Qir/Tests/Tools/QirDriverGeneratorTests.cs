69d29c5332fe6677f2c1c37a8550f613336827f6
Sarah Marshall <sarah.marshall@microsoft.com>
2021-09-14 12:55:24 -0700

    Use single dash for single letter command line options

--- src/Qir/Tests/Tools/QirDriverGeneratorTests.cs
+++ src/Qir/Tests/Tools/QirDriverGeneratorTests.cs
@@ -180,5 +180,51 @@ namespace Tests.Microsoft.Quantum.Qir.Runtime.Tools
             Assert.Equal(verificationCppSourceCode, generatedCppSourceCode);
             generatedStream.Close();
         }
+
+        [Theory]
+        [MemberData(nameof(GenerateCommandLineArgumentsData))]
+        public void GenerateCommandLineArguments(string expected, ExecutionInformation info)
+        {
+            var generator = new QirFullStateDriverGenerator(false);
+            Assert.Equal(expected, generator.GetCommandLineArguments(info));
+        }
+
+        public static IEnumerable<object[]> GenerateCommandLineArgumentsData()
+        {
+            static object[] TestCase(
+                string expected, List<Parameter> parameters, Dictionary<string, ArgumentValue> arguments) =>
+                new object[]
+                {
+                    expected,
+                    new ExecutionInformation
+                    {
+                        EntryPoint = new EntryPointOperation { Parameters = parameters },
+                        ArgumentValues = arguments
+                    }
+                };
+
+            yield return TestCase(
+                "--foo 5",
+                new List<Parameter> { new Parameter { Name = "foo", Position = 0, Type = DataType.IntegerType } },
+                new Dictionary<string, ArgumentValue> { ["foo"] = new ArgumentValue { Type = DataType.IntegerType, Integer = 5 } });
+
+            yield return TestCase(
+                "-n 5",
+                new List<Parameter> { new Parameter { Name = "n", Position = 0, Type = DataType.IntegerType } },
+                new Dictionary<string, ArgumentValue> { ["n"] = new ArgumentValue { Type = DataType.IntegerType, Integer = 5 } });
+
+            yield return TestCase(
+                "--foo 5 --bar \"abc\"",
+                new List<Parameter>
+                {
+                    new Parameter { Name = "bar", Position = 1, Type = DataType.StringType },
+                    new Parameter { Name = "foo", Position = 0, Type = DataType.IntegerType }
+                },
+                new Dictionary<string, ArgumentValue>
+                {
+                    ["bar"] = new ArgumentValue { Type = DataType.StringType, String = "abc" },
+                    ["foo"] = new ArgumentValue { Type = DataType.IntegerType, Integer = 5 }
+                });
+        }
     }
 }
