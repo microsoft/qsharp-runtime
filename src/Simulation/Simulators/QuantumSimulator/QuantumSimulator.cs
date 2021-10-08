8d57994dad50fa4f3e249cc909ad696cf8479941
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-10-07 18:14:23 -0700

    Added QuantumSimulator/QuantumSimulator.cs

---
+++ src/Simulation/Simulators/QuantumSimulator/QuantumSimulator.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Common;
+using System.Runtime.InteropServices;
+using Microsoft.Quantum.Simulation.Simulators.Exceptions;
+using Microsoft.Quantum.Intrinsic.Interfaces;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Microsoft.Quantum.Simulation.Simulators
+{
+    public partial class QuantumSimulator : NativeCommonSimulator
+    {
+        /// <summary>
+        /// Creates a an instance of a quantum simulator.
+        /// </summary>
+        /// <param name="throwOnReleasingQubitsNotInZeroState"> If set to true, the exception is thrown when trying to release qubits not in zero state. </param>
+        /// <param name="randomNumberGeneratorSeed"> Seed for the random number generator used by a simulator for measurement outcomes and the Random operation. </param>
+        /// <param name="disableBorrowing"> If true, Borrowing qubits will be disabled, and a new qubit will be allocated instead every time borrowing is requested. Performance may improve. </param>
+        public QuantumSimulator(
+            bool throwOnReleasingQubitsNotInZeroState = true,
+            UInt32? randomNumberGeneratorSeed = null,
+            bool disableBorrowing = false)
+        : base(throwOnReleasingQubitsNotInZeroState,
+               randomNumberGeneratorSeed,
+               disableBorrowing)
+        {
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "QuantumSimulator";
+            }
+        }
+    }
+}
