8297a8499e601888119c2f051bd771cf9146c38b
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-10-13 13:48:02 -0700

    Made NativeCommonSimulator ctor private protected

--- src/Simulation/Simulators/NativeCommonSimulator/NativeCommonSimulator.cs
+++ src/Simulation/Simulators/NativeCommonSimulator/NativeCommonSimulator.cs
@@ -20,7 +20,7 @@ namespace Microsoft.Quantum.Simulation.Simulators
         /// <param name="throwOnReleasingQubitsNotInZeroState"> If set to true, the exception is thrown when trying to release qubits not in zero state. </param>
         /// <param name="randomNumberGeneratorSeed"> Seed for the random number generator used by a simulator for measurement outcomes and the Random operation. </param>
         /// <param name="disableBorrowing"> If true, Borrowing qubits will be disabled, and a new qubit will be allocated instead every time borrowing is requested. Performance may improve. </param>
-        public NativeCommonSimulator(
+        private protected NativeCommonSimulator(
             bool throwOnReleasingQubitsNotInZeroState = true,
             UInt32? randomNumberGeneratorSeed = null,
             bool disableBorrowing = false)
