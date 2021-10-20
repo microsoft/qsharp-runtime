af99b9dceae43fab91a94836124c3a7a23e04a61
XField <58103249+vxfield@users.noreply.github.com>
2021-10-20 08:30:45 -0700

    Update the static AppId to Q#Runtime

--- src/Simulation/EntryPointDriver/Azure/AzureSettings.cs
+++ src/Simulation/EntryPointDriver/Azure/AzureSettings.cs
@@ -136,7 +136,7 @@ namespace Microsoft.Quantum.EntryPointDriver
 
             // This value will be added as a prefix in the UserAgent when
             // calling the Azure Quantum API
-            options.Diagnostics.ApplicationId = string.Join(' ', "Q#Sim", UserAgent);
+            options.Diagnostics.ApplicationId = string.Join(' ', "Q#Runtime", UserAgent);
             return options;
         }
 
