3e196c115dbe17793a29dc87f0be565033ff9d3c
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-11-17 13:25:57 -0800

    Sanitizing NativeSimulator

--- src/Simulation/Native/build-native-simulator.ps1
+++ src/Simulation/Native/build-native-simulator.ps1
@@ -28,7 +28,12 @@ elseif (($IsMacOS) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith(
     Write-Host "On MacOS build native simulator using gcc (needed for OpenMP)"
     # `gcc`on Darwin seems to be a shim that redirects to AppleClang, to get real gcc, must point to the specific
     # version of gcc we've installed.
-    cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_C_COMPILER=gcc-11 -D CMAKE_CXX_COMPILER=g++-11 -D CMAKE_BUILD_TYPE="Release" ..
+    $sanitizeFlags = " -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=unsigned-integer-overflow -fsanitize=implicit-conversion -fsanitize=local-bounds -fsanitize=nullability"
+    $sanitizeFlags += " -fsanitize=address"
+    $sanitizeFlags += " -fno-omit-frame-pointer"
+    $sanitizeFlags += " -fno-optimize-sibling-calls"
+
+    cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_C_COMPILER=gcc-11 -D CMAKE_CXX_COMPILER=g++-11 -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" -D CMAKE_CXX_FLAGS_DEBUG="$sanitizeFlags" -D CMAKE_C_FLAGS_DEBUG="$sanitizeFlags" ..
 }
 else {
     cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" ..
