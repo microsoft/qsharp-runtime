10250cdb124369b115721162ff3d2b8f246f2b44
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-11-17 14:09:40 -0800

    Added sanitizer for Linux to run locally on WSL

--- src/Simulation/Native/build-native-simulator.ps1
+++ src/Simulation/Native/build-native-simulator.ps1
@@ -10,6 +10,12 @@ if (-not (Test-Path $nativeBuild)) {
 }
 Push-Location $nativeBuild
 
+    $sanitizeFlags = " -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=unsigned-integer-overflow -fsanitize=implicit-conversion -fsanitize=local-bounds -fsanitize=nullability"
+    $sanitizeFlags += " -fsanitize=address"
+    $sanitizeFlags += " -fno-omit-frame-pointer"
+    $sanitizeFlags += " -fno-optimize-sibling-calls"
+
+
 # There should be no space after -D CMAKE_BUILD_TYPE= but if we provide the environment variable inline, without
 # the space it doesn't seem to get substituted... With invalid -D CMAKE_BUILD_TYPE argument cmake silently produces
 # a DEBUG build.
@@ -21,18 +27,13 @@ if (($IsWindows) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("W
 elseif (($IsLinux) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Lin"))))
 {
     Write-Host "On Linux build native simulator using gcc (needed for OpenMP)"
-    cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" ..
+    cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" -D CMAKE_CXX_FLAGS_DEBUG="$sanitizeFlags" -D CMAKE_C_FLAGS_DEBUG="$sanitizeFlags" ..
 }
 elseif (($IsMacOS) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Darwin"))))
 {
     Write-Host "On MacOS build native simulator using gcc (needed for OpenMP)"
     # `gcc`on Darwin seems to be a shim that redirects to AppleClang, to get real gcc, must point to the specific
     # version of gcc we've installed.
-    $sanitizeFlags = " -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=unsigned-integer-overflow -fsanitize=implicit-conversion -fsanitize=local-bounds -fsanitize=nullability"
-    $sanitizeFlags += " -fsanitize=address"
-    $sanitizeFlags += " -fno-omit-frame-pointer"
-    $sanitizeFlags += " -fno-optimize-sibling-calls"
-
     cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_C_COMPILER=gcc-11 -D CMAKE_CXX_COMPILER=g++-11 -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" -D CMAKE_CXX_FLAGS_DEBUG="$sanitizeFlags" -D CMAKE_C_FLAGS_DEBUG="$sanitizeFlags" ..
 }
 else {
