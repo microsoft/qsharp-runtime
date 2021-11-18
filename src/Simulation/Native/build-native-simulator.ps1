30790b310950384e64a471982d83157c13b3a9ad
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-11-17 18:44:58 -0800

    Fixed the sanitizer options.

--- src/Simulation/Native/build-native-simulator.ps1
+++ src/Simulation/Native/build-native-simulator.ps1
@@ -10,10 +10,21 @@ if (-not (Test-Path $nativeBuild)) {
 }
 Push-Location $nativeBuild
 
-    $sanitizeFlags = " -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=unsigned-integer-overflow -fsanitize=implicit-conversion -fsanitize=local-bounds -fsanitize=nullability"
-    $sanitizeFlags += " -fsanitize=address"
-    $sanitizeFlags += " -fno-omit-frame-pointer"
-    $sanitizeFlags += " -fno-optimize-sibling-calls"
+    $sanitizeFlags = ""
+
+    #$sanitizeFlags += " -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=unsigned-integer-overflow -fsanitize=implicit-conversion -fsanitize=local-bounds -fsanitize=nullability"
+    #$sanitizeFlags += " -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=signed-integer-overflow "
+    #$sanitizeFlags += " -fsanitize=address"
+    #$sanitizeFlags += " -fno-omit-frame-pointer"
+    #$sanitizeFlags += " -fno-optimize-sibling-calls"
+
+    $sanitizeFlags += " -fsanitize=undefined -fsanitize=shift -fsanitize=shift-exponent -fsanitize=shift-base -fsanitize=integer-divide-by-zero -fsanitize=unreachable -fsanitize=vla-bound "
+    $sanitizeFlags +=   " -fsanitize=null -fsanitize=return -fsanitize=signed-integer-overflow -fsanitize=bounds -fsanitize=bounds-strict -fsanitize=alignment -fsanitize=object-size "
+    $sanitizeFlags +=   " -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fsanitize=nonnull-attribute -fsanitize=returns-nonnull-attribute -fsanitize=bool -fsanitize=enum "
+    $sanitizeFlags +=   " -fsanitize=vptr -fsanitize=pointer-overflow -fsanitize=builtin "
+
+    #$sanitizeFlags +=   " -fsanitize=address -fsanitize=pointer-compare -fsanitize=pointer-subtract "
+
 
 
 # There should be no space after -D CMAKE_BUILD_TYPE= but if we provide the environment variable inline, without
@@ -27,7 +38,8 @@ if (($IsWindows) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("W
 elseif (($IsLinux) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Lin"))))
 {
     Write-Host "On Linux build native simulator using gcc (needed for OpenMP)"
-    cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" -D CMAKE_CXX_FLAGS_DEBUG="$sanitizeFlags" -D CMAKE_C_FLAGS_DEBUG="$sanitizeFlags" ..
+    cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" ..
+    #cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" -D CMAKE_CXX_FLAGS_DEBUG="$sanitizeFlags" -D CMAKE_C_FLAGS_DEBUG="$sanitizeFlags" -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
 }
 elseif (($IsMacOS) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Darwin"))))
 {
