a3300309f114c3548041cc5a8d78a9832fbe71e5
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-11-24 20:25:21 -0800

    Build NS first, sanitizedrs optimization

--- src/Qir/Tests/test-qir-tests.ps1
+++ src/Qir/Tests/test-qir-tests.ps1
@@ -24,7 +24,7 @@ if ($Env:BUILD_CONFIGURATION -eq "Debug")
         #{
         #    $env:ASAN_OPTIONS += "detect_leaks=1"      # https://clang.llvm.org/docs/AddressSanitizer.html#memory-leak-detection
         #}
-        $env:ASAN_OPTIONS = "check_initialization_order=true:detect_stack_use_after_return=true:print_stats=true:" `
+        $env:ASAN_OPTIONS = "check_initialization_order=true:detect_stack_use_after_return=true:" `
             + "alloc_dealloc_mismatch=true:new_delete_type_mismatch=true:strict_init_order=true:strict_string_checks=true:" `
             + "detect_invalid_pointer_pairs=2:detect_invalid_pointer_pairs=2"
     }
