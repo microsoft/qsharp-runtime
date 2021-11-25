2f4bd873f036d383fb76346fe92a1b958b7b5703
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-11-24 18:46:37 -0800

    Added sanitizers to NS.

--- src/Simulation/Native/build-native-simulator.ps1
+++ src/Simulation/Native/build-native-simulator.ps1
@@ -10,6 +10,12 @@ if (-not (Test-Path $nativeBuild)) {
 }
 Push-Location $nativeBuild
 
+$SANITIZE_FLAGS="-fsanitize=undefined -fsanitize=float-divide-by-zero " `
+    + "-fsanitize=unsigned-integer-overflow -fsanitize=implicit-conversion -fsanitize=local-bounds -fsanitize=nullability " `
+    + "-fsanitize=address " `
+    + "-fsanitize-blacklist=/Users/runner/work/1/s/src/Qir/Common/cmake/../../UBSan.ignore " `
+    + "-fno-omit-frame-pointer -fno-optimize-sibling-calls"
+
 # There should be no space after -D CMAKE_BUILD_TYPE= but if we provide the environment variable inline, without
 # the space it doesn't seem to get substituted... With invalid -D CMAKE_BUILD_TYPE argument cmake silently produces
 # a DEBUG build.
@@ -22,7 +28,10 @@ if (($IsWindows) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("W
 elseif (($IsLinux) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Lin"))))
 {
     Write-Host "On Linux build native simulator using gcc (needed for OpenMP)"
-    cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_C_COMPILER=clang-11 -D CMAKE_CXX_COMPILER=clang++-11 -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" -D CMAKE_VERBOSE_MAKEFILE:BOOL="1" ..
+    cmake -D BUILD_SHARED_LIBS:BOOL="1" -D CMAKE_C_COMPILER=clang-11 -D CMAKE_CXX_COMPILER=clang++-11 `
+        -D CMAKE_C_FLAGS_DEBUG="$SANITIZE_FLAGS" `
+        -D CMAKE_CXX_FLAGS_DEBUG="$SANITIZE_FLAGS" `
+        -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" -D CMAKE_VERBOSE_MAKEFILE:BOOL="1" ..
     # TODO(rokuzmin): Updte prerequisites.
 }
 elseif (($IsMacOS) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Darwin"))))
@@ -39,6 +48,8 @@ elseif (($IsMacOS) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith(
         -D OpenMP_CXX_FLAGS="$OPENMP_COMPILER_FLAGS" -D OpenMP_CXX_LIB_NAMES="$OPENMP_LIB_NAME" `
         -D   OpenMP_C_FLAGS="$OPENMP_COMPILER_FLAGS" -D   OpenMP_C_LIB_NAMES="$OPENMP_LIB_NAME" `
         -D OpenMP_omp_LIBRARY=$OPENMP_PATH/lib/libomp.dylib `
+        -D CMAKE_C_FLAGS_DEBUG="$SANITIZE_FLAGS" `
+        -D CMAKE_CXX_FLAGS_DEBUG="$SANITIZE_FLAGS" `
         -D CMAKE_BUILD_TYPE="$Env:BUILD_CONFIGURATION" -D CMAKE_VERBOSE_MAKEFILE:BOOL="1" ..
     # TODO(rokuzmin): Updte prerequisites.
 }
