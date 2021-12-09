d169bde827d2862ad6e62a7bbbc037bbe0443628
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-12-09 12:45:34 -0800

    A fake change commit (space only change in a comment) to trigger the CI build from scratch.

--- src/Qir/Runtime/public/QubitManager.hpp
+++ src/Qir/Runtime/public/QubitManager.hpp
@@ -42,7 +42,7 @@ namespace Quantum
         CQubitManager& operator=(const CQubitManager&) = delete;
         ~CQubitManager(); // If this dtor is made _virtual_ then the QIR RT tests crash (at least in Debug config)
                           // if the native simulator is compiled with Clang (as opposed to GCC). Nothing wrong found in
-                          // the code, probably is the compiler bug.
+                          // the code,  probably is the compiler bug.
 
         // Restricted reuse area control
         void StartRestrictedReuseArea();
