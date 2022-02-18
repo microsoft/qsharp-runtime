66e76a8f460bb5516ab4ada652d54f372fecf0e5
César Zaragoza Cortés <cesar.zaragoza@outlook.com>
2022-02-18 10:57:25 -0800

    Fix quantum machine factory.

--- src/Azure/Azure.Quantum.Client/Machine/QuantumMachineFactory.cs
+++ src/Azure/Azure.Quantum.Client/Machine/QuantumMachineFactory.cs
@@ -29,12 +29,14 @@ namespace Microsoft.Azure.Quantum
             var machineName =
                 targetNameNormalized is null
                 ? null
+                : targetNameNormalized.StartsWith("quantinuum.")
+                ? "Microsoft.Quantum.Providers.Quantinuum.Targets.QuantinuumQuantumMachine, Microsoft.Quantum.Providers.Honeywell"
                 : targetNameNormalized.StartsWith("qci.")
                 ? "Microsoft.Quantum.Providers.QCI.Targets.QCIQuantumMachine, Microsoft.Quantum.Providers.QCI"
                 : targetNameNormalized.StartsWith("ionq.")
                 ? "Microsoft.Quantum.Providers.IonQ.Targets.IonQQuantumMachine, Microsoft.Quantum.Providers.IonQ"
                 : targetNameNormalized.StartsWith("honeywell.")
-                ? "Microsoft.Quantum.Providers.Honeywell.Targets.QuantinuumQuantumMachine, Microsoft.Quantum.Providers.Honeywell"
+                ? "Microsoft.Quantum.Providers.Honeywell.Targets.HoneywellQuantumMachine, Microsoft.Quantum.Providers.Honeywell"
                 : null;
 
             Type? machineType = null;
