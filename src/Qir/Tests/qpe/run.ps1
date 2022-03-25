54e202a429727fd106bf43dc08b988b7f62bf949
Robin Kuzmin <kuzmin.robin@gmail.com>
2022-03-24 22:31:31 -0700

    WSL fixes.

--- src/Qir/Tests/qpe/run.ps1
+++ src/Qir/Tests/qpe/run.ps1
@@ -1,1 +1,5 @@
-Invoke-Expression ".\build\qpe.exe py_out.json"
+$OldLD_LIBRARY_PATH = $Env:LD_LIBRARY_PATH
+    $Env:LD_LIBRARY_PATH = "$PSScriptRoot/build:$Env:LD_LIBRARY_PATH"
+    #Write-Host "$Env:LD_LIBRARY_PATH"
+    Invoke-Expression "./build/qpe py_out.json"
+$Env:LD_LIBRARY_PATH = $OldLD_LIBRARY_PATH
