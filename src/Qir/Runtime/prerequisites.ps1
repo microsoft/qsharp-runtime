c3b7c3a8589530552edadfe4f82e78d213e20364
Angela Burton <anjbur@users.noreply.github.com>
2022-04-26 12:05:52 -0700

    Add DevSkim scanning (#996)
    * Add DevSkim scanning
    
    * Resolve alert
    
    * Resolve alert

--- src/Qir/Runtime/prerequisites.ps1
+++ src/Qir/Runtime/prerequisites.ps1
@@ -32,14 +32,14 @@ if ($Env:ENABLE_QIRRUNTIME -ne "false") {
         if (Get-Command sudo -ErrorAction SilentlyContinue) {
             if ($needClang) { 
                 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-                sudo add-apt-repository "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-13 main"
+                sudo add-apt-repository "deb https://apt.llvm.org/focal/ llvm-toolchain-focal-13 main"
             }
             sudo apt update
             sudo apt-get install -y ninja-build clang-13 clang-tidy-13 clang-format-13
         } else {
             if ($needClang) {
                 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|apt-key add -
-                add-apt-repository "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-13 main"
+                add-apt-repository "deb https://apt.llvm.org/focal/ llvm-toolchain-focal-13 main"
             }
             apt update
             apt-get install -y ninja-build clang-13 clang-tidy-13 clang-format-13
