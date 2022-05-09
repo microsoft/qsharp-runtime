2186034230fdf8fd9e8570f8ab9b5d2514f4d998
Cassandra Granade <cgranade@cgranade.com>
2022-05-09 08:28:37 -0700

    Slight code cleanups.

--- src/Simulation/Simulators/OpenSystemsSimulator/DataModel/DelegatedConverter.cs
+++ src/Simulation/Simulators/OpenSystemsSimulator/DataModel/DelegatedConverter.cs
@@ -3,32 +3,24 @@
 
 #nullable enable
 
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Numerics;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using NumSharp;
 
-namespace Microsoft.Quantum.Experimental
+namespace Microsoft.Quantum.Experimental;
+
+public class DelegatedConverter<T> : Newtonsoft.Json.JsonConverter<T>
 {
-    public class DelegatedConverter<T> : Newtonsoft.Json.JsonConverter<T>
+    public override T ReadJson(JsonReader reader, Type objectType, [AllowNull] T existingValue, bool hasExistingValue, Newtonsoft.Json.JsonSerializer serializer)
     {
-        public override T ReadJson(JsonReader reader, Type objectType, [AllowNull] T existingValue, bool hasExistingValue, Newtonsoft.Json.JsonSerializer serializer)
-        {
-            var serialized = JToken.ReadFrom(reader).ToString();
-            return System.Text.Json.JsonSerializer.Deserialize<T>(serialized);
-        }
+        var serialized = JToken.ReadFrom(reader).ToString();
+        return System.Text.Json.JsonSerializer.Deserialize<T>(serialized);
+    }
 
-        public override void WriteJson(JsonWriter writer, [AllowNull] T value, Newtonsoft.Json.JsonSerializer serializer)
-        {
-            var serialized = System.Text.Json.JsonSerializer.Serialize(value);
-            var deserialized = Newtonsoft.Json.Linq.JToken.Parse(serialized);
-            deserialized.WriteTo(writer);
-        }
+    public override void WriteJson(JsonWriter writer, [AllowNull] T value, Newtonsoft.Json.JsonSerializer serializer)
+    {
+        var serialized = System.Text.Json.JsonSerializer.Serialize(value);
+        var deserialized = Newtonsoft.Json.Linq.JToken.Parse(serialized);
+        deserialized.WriteTo(writer);
     }
 }
--- src/Simulation/Simulators/OpenSystemsSimulator/DataModel/State.cs
+++ src/Simulation/Simulators/OpenSystemsSimulator/DataModel/State.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Numerics;
-using System.Text.Json;
 using System.Text.Json.Serialization;
 using NumSharp;
 using static System.Math;
