a36bb1b4cd0d54bfd7705b4cbda1a803910e6ad0
Robin Kuzmin <kuzmin.robin@gmail.com>
2022-05-11 18:03:43 -0700

    CR changes.

--- src/Simulation/EntryPointDriver/Azure/Azure.cs
+++ src/Simulation/EntryPointDriver/Azure/Azure.cs
@@ -432,9 +432,8 @@ namespace Microsoft.Quantum.EntryPointDriver
             {
                 NoOpQirSubmitter.Target => new NoOpQirSubmitter(),
                 NoOpSubmitter.Target => new NoOpSubmitter(),
-                _ => (!string.IsNullOrWhiteSpace(settings.TargetCapability)) 
-                    ? SubmitterFactory.QirSubmitter(settings.Target, settings.CreateWorkspace(), settings.Storage, settings.TargetCapability) 
-                    : null
+                _ => SubmitterFactory.QirSubmitter(settings.Target, settings.CreateWorkspace(), settings.Storage, 
+                                                   settings.TargetCapability)
             };
         }
 
