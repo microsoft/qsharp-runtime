07d3ff628580cd2c98e9dfaa802695b026710b79
Cassandra Granade <cgranade@cgranade.com>
2022-05-12 13:48:39 -0700

    Minor code cleanup.

--- src/Simulation/Simulators/OpenSystemsSimulator/NativeImplementations.cs
+++ src/Simulation/Simulators/OpenSystemsSimulator/NativeImplementations.cs
@@ -1,155 +1,141 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
-using System.Linq;
 using Microsoft.Quantum.Simulation.Core;
 using Microsoft.Quantum.Simulation.Common;
-using System.Runtime.InteropServices;
-using System.Threading;
-using Microsoft.Quantum.Simulation.Simulators.Exceptions;
-using Microsoft.Quantum.Intrinsic.Interfaces;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Newtonsoft.Json.Linq;
 
-using Native = Microsoft.Quantum.Experimental.Native;
-
-namespace Microsoft.Quantum.Experimental
+namespace Microsoft.Quantum.Experimental;
+public partial class OpenSystemsSimulator : SimulatorBase
 {
-    public partial class OpenSystemsSimulator : SimulatorBase
+    internal class H : Native.H
     {
+        public H(IOperationFactory m) : base(m) { }
 
-        internal class H : Native.H
+        public override Func<Qubit, QVoid> __Body__ => (target) =>
         {
-            public H(IOperationFactory m) : base(m) { }
+            NativeInterface.H((this.__Factory__ as OpenSystemsSimulator).Id, target);
+            return QVoid.Instance;
+        };
+    }
 
-            public override Func<Qubit, QVoid> __Body__ => (target) =>
-            {
-                NativeInterface.H((this.__Factory__ as OpenSystemsSimulator).Id, target);
-                return QVoid.Instance;
-            };
-        }
+    internal class X : Native.X
+    {
+        public X(IOperationFactory m) : base(m) { }
 
-        internal class X : Native.X
+        public override Func<Qubit, QVoid> __Body__ => (target) =>
         {
-            public X(IOperationFactory m) : base(m) { }
+            NativeInterface.X((this.__Factory__ as OpenSystemsSimulator).Id, target);
+            return QVoid.Instance;
+        };
+    }
 
-            public override Func<Qubit, QVoid> __Body__ => (target) =>
-            {
-                NativeInterface.X((this.__Factory__ as OpenSystemsSimulator).Id, target);
-                return QVoid.Instance;
-            };
-        }
+    internal class Y : Native.Y
+    {
+        public Y(IOperationFactory m) : base(m) { }
 
-        internal class Y : Native.Y
+        public override Func<Qubit, QVoid> __Body__ => (target) =>
         {
-            public Y(IOperationFactory m) : base(m) { }
+            NativeInterface.Y((this.__Factory__ as OpenSystemsSimulator).Id, target);
+            return QVoid.Instance;
+        };
+    }
 
-            public override Func<Qubit, QVoid> __Body__ => (target) =>
-            {
-                NativeInterface.Y((this.__Factory__ as OpenSystemsSimulator).Id, target);
-                return QVoid.Instance;
-            };
-        }
+    internal class Z : Native.Z
+    {
+        public Z(IOperationFactory m) : base(m) { }
 
-        internal class Z : Native.Z
+        public override Func<Qubit, QVoid> __Body__ => (target) =>
         {
-            public Z(IOperationFactory m) : base(m) { }
+            NativeInterface.Z((this.__Factory__ as OpenSystemsSimulator).Id, target);
+            return QVoid.Instance;
+        };
+    }
 
-            public override Func<Qubit, QVoid> __Body__ => (target) =>
-            {
-                NativeInterface.Z((this.__Factory__ as OpenSystemsSimulator).Id, target);
-                return QVoid.Instance;
-            };
-        }
+    internal class S : Native.S
+    {
+        public S(IOperationFactory m) : base(m) { }
 
-        internal class S : Native.S
+        public override Func<Qubit, QVoid> __Body__ => (target) =>
         {
-            public S(IOperationFactory m) : base(m) { }
+            NativeInterface.S((this.__Factory__ as OpenSystemsSimulator).Id, target);
+            return QVoid.Instance;
+        };
 
-            public override Func<Qubit, QVoid> __Body__ => (target) =>
-            {
-                NativeInterface.S((this.__Factory__ as OpenSystemsSimulator).Id, target);
-                return QVoid.Instance;
-            };
-
-            public override Func<Qubit, QVoid> __AdjointBody__ => (target) =>
-            {
-                NativeInterface.SAdj((this.__Factory__ as OpenSystemsSimulator).Id, target);
-                return QVoid.Instance;
-            };
-        }
-
-        internal class T : Native.T
+        public override Func<Qubit, QVoid> __AdjointBody__ => (target) =>
         {
-            public T(IOperationFactory m) : base(m) { }
+            NativeInterface.SAdj((this.__Factory__ as OpenSystemsSimulator).Id, target);
+            return QVoid.Instance;
+        };
+    }
 
-            public override Func<Qubit, QVoid> __Body__ => (target) =>
-            {
-                NativeInterface.T((this.__Factory__ as OpenSystemsSimulator).Id, target);
-                return QVoid.Instance;
-            };
+    internal class T : Native.T
+    {
+        public T(IOperationFactory m) : base(m) { }
 
-            public override Func<Qubit, QVoid> __AdjointBody__ => (target) =>
-            {
-                NativeInterface.TAdj((this.__Factory__ as OpenSystemsSimulator).Id, target);
-                return QVoid.Instance;
-            };
-        }
-
-        internal class CNOT : Native.CNOT
+        public override Func<Qubit, QVoid> __Body__ => (target) =>
         {
-            public CNOT(IOperationFactory m) : base(m) { }
+            NativeInterface.T((this.__Factory__ as OpenSystemsSimulator).Id, target);
+            return QVoid.Instance;
+        };
 
-            public override Func<(Qubit, Qubit), QVoid> __Body__ => (args) =>
-            {
-                NativeInterface.CNOT((this.__Factory__ as OpenSystemsSimulator).Id, args.Item1, args.Item2);
-                return QVoid.Instance;
-            };
-        }
-
-        internal class Rx : Native.Rx
+        public override Func<Qubit, QVoid> __AdjointBody__ => (target) =>
         {
-            public Rx(IOperationFactory m) : base(m) { }
+            NativeInterface.TAdj((this.__Factory__ as OpenSystemsSimulator).Id, target);
+            return QVoid.Instance;
+        };
+    }
 
-            public override Func<(double, Qubit), QVoid> __Body__ => (args) =>
-            {
-                NativeInterface.Rx((this.__Factory__ as OpenSystemsSimulator).Id, args.Item1, args.Item2);
-                return QVoid.Instance;
-            };
-        }
+    internal class CNOT : Native.CNOT
+    {
+        public CNOT(IOperationFactory m) : base(m) { }
 
-        internal class Ry : Native.Ry
+        public override Func<(Qubit, Qubit), QVoid> __Body__ => (args) =>
         {
-            public Ry(IOperationFactory m) : base(m) { }
+            NativeInterface.CNOT((this.__Factory__ as OpenSystemsSimulator).Id, args.Item1, args.Item2);
+            return QVoid.Instance;
+        };
+    }
 
-            public override Func<(double, Qubit), QVoid> __Body__ => (args) =>
-            {
-                NativeInterface.Ry((this.__Factory__ as OpenSystemsSimulator).Id, args.Item1, args.Item2);
-                return QVoid.Instance;
-            };
-        }
+    internal class Rx : Native.Rx
+    {
+        public Rx(IOperationFactory m) : base(m) { }
 
-        internal class Rz : Native.Rz
+        public override Func<(double, Qubit), QVoid> __Body__ => (args) =>
         {
-            public Rz(IOperationFactory m) : base(m) { }
+            NativeInterface.Rx((this.__Factory__ as OpenSystemsSimulator).Id, args.Item1, args.Item2);
+            return QVoid.Instance;
+        };
+    }
 
-            public override Func<(double, Qubit), QVoid> __Body__ => (args) =>
-            {
-                NativeInterface.Rz((this.__Factory__ as OpenSystemsSimulator).Id, args.Item1, args.Item2);
-                return QVoid.Instance;
-            };
-        }
+    internal class Ry : Native.Ry
+    {
+        public Ry(IOperationFactory m) : base(m) { }
 
-        internal class M : Native.M
+        public override Func<(double, Qubit), QVoid> __Body__ => (args) =>
         {
-            public M(IOperationFactory m) : base(m) { }
+            NativeInterface.Ry((this.__Factory__ as OpenSystemsSimulator).Id, args.Item1, args.Item2);
+            return QVoid.Instance;
+        };
+    }
 
-            public override Func<Qubit, Result> __Body__ => (target) =>
-                NativeInterface.M((this.__Factory__ as OpenSystemsSimulator).Id, target);
-        }
+    internal class Rz : Native.Rz
+    {
+        public Rz(IOperationFactory m) : base(m) { }
 
+        public override Func<(double, Qubit), QVoid> __Body__ => (args) =>
+        {
+            NativeInterface.Rz((this.__Factory__ as OpenSystemsSimulator).Id, args.Item1, args.Item2);
+            return QVoid.Instance;
+        };
+    }
+
+    internal class M : Native.M
+    {
+        public M(IOperationFactory m) : base(m) { }
+
+        public override Func<Qubit, Result> __Body__ => (target) =>
+            NativeInterface.M((this.__Factory__ as OpenSystemsSimulator).Id, target);
     }
 
 }
+
