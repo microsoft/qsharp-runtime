5fff35b6b5761e9fec1d723fa2049849d4b06082
Robin Kuzmin <kuzmin.robin@gmail.com>
2022-05-15 16:37:47 -0700

    Reverted the change.

--- src/Simulation/Core/Submitters/SubmissionOptions.cs
+++ src/Simulation/Core/Submitters/SubmissionOptions.cs
@@ -28,23 +28,16 @@ namespace Microsoft.Quantum.Runtime.Submitters
         public ImmutableDictionary<string, string> InputParams { get; }
 
         /// <summary>
-        /// The target capability.
-        /// </summary>
-        public string TargetCapability { get; set; }
-
-        /// <summary>
         /// The default submission options.
         /// </summary>
         public static SubmissionOptions Default { get; } =
-            new SubmissionOptions("", 500, ImmutableDictionary<string, string>.Empty, "");
+            new SubmissionOptions("", 500, ImmutableDictionary<string, string>.Empty);
 
-        private SubmissionOptions(
-            string friendlyName, int shots, ImmutableDictionary<string, string> inputParams, string targetCapability)
+        private SubmissionOptions(string friendlyName, int shots, ImmutableDictionary<string, string> inputParams)
         {
             FriendlyName = friendlyName;
             Shots = shots;
             InputParams = inputParams;
-            TargetCapability = targetCapability;
         }
 
         /// <summary>
@@ -53,14 +46,11 @@ namespace Microsoft.Quantum.Runtime.Submitters
         /// <param name="friendlyName">The new friendly name, or <c>null</c> to leave unchanged.</param>
         /// <param name="shots">The new number of shots, or <c>null</c> to leave unchanged.</param>
         /// <param name="inputParams">The new input parameters, or <c>null</c> to leave unchanged.</param>
-        /// <param name="targetCapability">The target capability, or <c>null</c> to leave unchanged.</param>
         /// <returns>The updated submission options.</returns>
         public SubmissionOptions With(
             string? friendlyName = null,
             int? shots = null,
-            ImmutableDictionary<string, string>? inputParams = null,
-            string? targetCapability = null) =>
-            new SubmissionOptions(
-                friendlyName ?? FriendlyName, shots ?? Shots, inputParams ?? InputParams, targetCapability ?? TargetCapability);
+            ImmutableDictionary<string, string>? inputParams = null) =>
+            new SubmissionOptions(friendlyName ?? FriendlyName, shots ?? Shots, inputParams ?? InputParams);
     }
 }
