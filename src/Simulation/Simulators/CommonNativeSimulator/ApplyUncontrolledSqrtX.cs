a0ab34712d650f24dfa87a8609882aaa26213abb
Stefan J. Wernli <swernli@microsoft.com>
2022-07-03 23:47:39 -0700

    Add tests, fix Y and Ry decompositions

---
+++ src/Simulation/Simulators/CommonNativeSimulator/ApplyUncontrolledSqrtX.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Intrinsic.Interfaces;
+
+namespace Microsoft.Quantum.Simulation.Simulators
+{
+    public partial class CommonNativeSimulator
+    {
+        void IIntrinsicApplyUncontrolledSqrtX.Body(Qubit target)
+        {
+            this.CheckQubit(target);
+
+            H((uint)target.Id);
+            S((uint)target.Id);
+            H((uint)target.Id);
+        }
+    }
+}
