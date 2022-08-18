3a16d20c1a1fd0c4ce45a18d1827efa0bd9e2f41
Stefan J. Wernli <swernli@microsoft.com>
2022-08-18 10:40:10 -0700

    Explicitly treat ll symbols as undefined

--- src/Qir/Runtime/lib/QIR/build.rs
+++ src/Qir/Runtime/lib/QIR/build.rs
@@ -6,12 +6,16 @@ fn main() {
     let mut clang = cc::Build::new();
     if cfg!(target_os = "linux") {
         clang.compiler(which::which("clang-13").expect("Failed to find clang-13 in path!"));
+        println!("cargo:rustc-cdylib-link-arg=-undefined=__quantum__rt__array_slice_1d");
+        println!("cargo:rustc-cdylib-link-arg=-undefined=__quantum__rt__range_to_string");
     } else if cfg!(target_os = "windows") {
         clang.compiler(which::which("clang.exe").expect("Failed to find clang.exe in path!"));
         println!("cargo:rustc-cdylib-link-arg=/export:__quantum__rt__array_slice_1d");
         println!("cargo:rustc-cdylib-link-arg=/export:__quantum__rt__range_to_string");
     } else if cfg!(target_os = "macos") {
         // Use default compiler on MacOS
+        println!("cargo:rustc-cdylib-link-arg=-undefined=__quantum__rt__array_slice_1d");
+        println!("cargo:rustc-cdylib-link-arg=-undefined=__quantum__rt__range_to_string");
     }
     clang
         .file("src/bridge-rt.ll")
