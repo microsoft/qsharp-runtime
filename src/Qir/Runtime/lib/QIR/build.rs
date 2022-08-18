941855638580fc0fba90f035c56e0f2688478d32
Stefan J. Wernli <swernli@microsoft.com>
2022-08-18 14:17:30 -0700

    Try `-u` mac linking

--- src/Qir/Runtime/lib/QIR/build.rs
+++ src/Qir/Runtime/lib/QIR/build.rs
@@ -14,6 +14,8 @@ fn main() {
         println!("cargo:rustc-cdylib-link-arg=/export:__quantum__rt__range_to_string");
     } else if cfg!(target_os = "macos") {
         // Use default compiler on MacOS
+        println!("cargo:rustc-cdylib-link-arg=-u __quantum__rt__array_slice_1d");
+        println!("cargo:rustc-cdylib-link-arg=-u __quantum__rt__range_to_string");
     }
     clang
         .file("src/bridge-rt.ll")
