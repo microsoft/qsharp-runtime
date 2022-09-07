3f38b9e118df2fa9c4c4bd22963ae029be448d9f
Stefan J. Wernli <swernli@microsoft.com>
2022-09-07 16:19:29 -0700

    Remove unused qir_backend

--- src/Qir/Runtime/test-qir-stdlib.ps1
+++ src/Qir/Runtime/test-qir-stdlib.ps1
@@ -1,28 +1,26 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-foreach ($folder in "stdlib","backend") {
-    Push-Location (Join-Path $PSScriptRoot $folder)
-    try {
-        $releaseFlag = "$Env:BUILD_CONFIGURATION" -eq "Release" ? @("--release") : @();
+Push-Location (Join-Path $PSScriptRoot stdlib)
+try {
+    $releaseFlag = "$Env:BUILD_CONFIGURATION" -eq "Release" ? @("--release") : @();
 
-        # Enable control flow guard (see https://github.com/microsoft/qsharp-runtime/pull/647)
-        # for interoperating Rust and C.
-        # NB: CFG is only supported on Windows, but the Rust flag is supported on
-        #     all platforms; it's ignored on platforms without CFG functionality.
-        $Env:RUSTFLAGS = "-C control-flow-guard";
+    # Enable control flow guard (see https://github.com/microsoft/qsharp-runtime/pull/647)
+    # for interoperating Rust and C.
+    # NB: CFG is only supported on Windows, but the Rust flag is supported on
+    #     all platforms; it's ignored on platforms without CFG functionality.
+    $Env:RUSTFLAGS = "-C control-flow-guard";
 
-        # Actually run the test.
-        cargo test @releaseFlag
-        if ($LASTEXITCODE -ne 0) { throw "Failed cargo test on QIR $folder." }
+    # Actually run the test.
+    cargo test @releaseFlag
+    if ($LASTEXITCODE -ne 0) { throw "Failed cargo test on QIR stdlib." }
 
-        # When building in CI, free disk space by cleaning up.
-        # Note that this takes longer, but saves ~1 GB of space.
-        if ($IsCI) {
-            cargo clean;
-        }
+    # When building in CI, free disk space by cleaning up.
+    # Note that this takes longer, but saves ~1 GB of space.
+    if ($IsCI) {
+        cargo clean;
     }
-    finally {
-        Pop-Location
-    }
+}
+finally {
+    Pop-Location
 }
