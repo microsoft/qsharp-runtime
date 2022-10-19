d65acdd08b4feae3310f8bdd6468cb5c0f1760af
Rose <83477269+AtariDreams@users.noreply.github.com>
2022-10-19 10:43:00 -0400

    Fix

--- src/Qir/Runtime/prerequisites.ps1
+++ src/Qir/Runtime/prerequisites.ps1
@@ -11,7 +11,7 @@ if ($Env:ENABLE_QIRRUNTIME -ne "false") {
         if (!(Get-Command clang        -ErrorAction SilentlyContinue) -or `
             !(Get-Command clang-format -ErrorAction SilentlyContinue) -or `
             (Test-Path Env:/AGENT_OS)) {
-            choco install llvm --version=15.0.3 --allow-downgrade
+            choco install llvm --version=14.0.6 --allow-downgrade
             $ChocoRan = $true
             Write-Host "##vso[task.setvariable variable=PATH;]$($env:SystemDrive)\Program Files\LLVM\bin;$Env:PATH"
         }
