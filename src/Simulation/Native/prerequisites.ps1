be42183c4e228f9c73d30c5267a8ed1b6acaeee3
Robin Kuzmin <kuzmin.robin@gmail.com>
2022-11-28 13:01:09 -0800

    Switch Ubuntu from focal to jammy.

--- src/Simulation/Native/prerequisites.ps1
+++ src/Simulation/Native/prerequisites.ps1
@@ -29,38 +29,25 @@ if (($IsMacOS) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Dar
 }
 else {
     $needClang = !(Get-Command clang-14 -ErrorAction SilentlyContinue)
-    $Clang14PrefFilePath = "/etc/apt/preferences.d/clang-14.pref"
+    $UbuntuCodeName = "jammy"   # 22.04.1 LTS (Jammy Jellyfish) https://github.com/actions/runner-images/blob/main/images/linux/Ubuntu2204-Readme.md
+    #                 "focal"   # 20.04.5 LTS (Focal Fossa)     https://github.com/actions/runner-images/blob/main/images/linux/Ubuntu2004-Readme.md
     if (Get-Command sudo -ErrorAction SilentlyContinue) {
         if ($needClang) {
             wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-            sudo add-apt-repository "deb https://apt.llvm.org/focal/ llvm-toolchain-focal-14 main"
+            sudo add-apt-repository "deb https://apt.llvm.org/$UbuntuCodeName/ llvm-toolchain-$UbuntuCodeName-14 main"
         }
         sudo apt update
         sudo apt-get install -y ninja-build
         sudo apt-get install -y libomp-14-dev
-
-        # man 5 apt_preferences
-        sudo sh -c "echo 'Package: clang-14'   >   $Clang14PrefFilePath"
-        sudo sh -c "echo 'Pin: version 14.0.6' >>  $Clang14PrefFilePath"
-        sudo sh -c "echo 'Pin-Priority: 1001'  >>  $Clang14PrefFilePath"
-        sudo cat $Clang14PrefFilePath
-
         sudo apt-get install -y clang-14
     } else {
         if ($needClang) {
             wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|apt-key add -
-            add-apt-repository "deb https://apt.llvm.org/focal/ llvm-toolchain-focal-14 main"
+            add-apt-repository "deb https://apt.llvm.org/$UbuntuCodeName/ llvm-toolchain-$UbuntuCodeName-14 main"
         }
         apt update
         apt-get install -y ninja-build
         apt-get install -y libomp-14-dev
-
-        # man 5 apt_preferences
-        echo 'Package: clang-14'   >   $Clang14PrefFilePath
-        echo 'Pin: version 14.0.6' >>  $Clang14PrefFilePath
-        echo 'Pin-Priority: 1001'  >>  $Clang14PrefFilePath
-        cat $Clang14PrefFilePath
-
         apt-get install -y clang-14
     }
 }
