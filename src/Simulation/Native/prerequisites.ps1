0e5e6f06613411b7ca24e392b37694a54d050423
Robin Kuzmin <kuzmin.robin@gmail.com>
2022-11-28 11:52:32 -0800

    Fixing the clang version to 14.0.6.

--- src/Simulation/Native/prerequisites.ps1
+++ src/Simulation/Native/prerequisites.ps1
@@ -29,6 +29,7 @@ if (($IsMacOS) -or ((Test-Path Env:AGENT_OS) -and ($Env:AGENT_OS.StartsWith("Dar
 }
 else {
     $needClang = !(Get-Command clang-14 -ErrorAction SilentlyContinue)
+    $Clang14PrefFilePath = "/etc/apt/preferences.d/clang-14.pref"
     if (Get-Command sudo -ErrorAction SilentlyContinue) {
         if ($needClang) {
             wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
@@ -37,6 +38,13 @@ else {
         sudo apt update
         sudo apt-get install -y ninja-build
         sudo apt-get install -y libomp-14-dev
+
+        # man 5 apt_preferences
+        sudo sh -c "echo 'Package: clang-14'   >   $Clang14PrefFilePath"
+        sudo sh -c "echo 'Pin: version 14.0.6' >>  $Clang14PrefFilePath"
+        sudo sh -c "echo 'Pin-Priority: 1001'  >>  $Clang14PrefFilePath"
+        sudo cat $Clang14PrefFilePath
+
         sudo apt-get install -y clang-14
     } else {
         if ($needClang) {
@@ -46,9 +54,13 @@ else {
         apt update
         apt-get install -y ninja-build
         apt-get install -y libomp-14-dev
+
+        # man 5 apt_preferences
+        echo 'Package: clang-14'   >   $Clang14PrefFilePath
+        echo 'Pin: version 14.0.6' >>  $Clang14PrefFilePath
+        echo 'Pin-Priority: 1001'  >>  $Clang14PrefFilePath
+        cat $Clang14PrefFilePath
+
         apt-get install -y clang-14
     }
 }
-
-
-
