2a128d6ed8986d7cccd793a9039032e8cbfe8502
Stefan J. Wernli <swernli@microsoft.com>
2022-12-08 15:07:32 -0800

    Use qir-stdlib from the qir-runner repo (#1111)
    This change removes the code for the qir-stdlib and instead compiles a wrapper library for it, allowing the source code from https://github.com/qir-alliance/qir-runner to the be authoritative implementation.

--- src/Qir/Runtime/build-qir-stdlib.ps1
+++ src/Qir/Runtime/build-qir-stdlib.ps1
@@ -32,23 +32,19 @@ try {
 
     # Copy the results of compilation and the corresponding headers to the QIR drops folder so
     # they can be included in pipeline artifacts.
-    $qirDropsBin = (Join-Path $Env:QIR_DROPS bin $env:BUILD_PLATFORM native)
     $qirDropsInclude = (Join-Path $Env:QIR_DROPS include)
     if (-not (Test-Path $Env:QIR_DROPS)) {
         New-Item -Path $Env:QIR_DROPS -ItemType "directory"
-    }
-    if (-not (Test-Path $qirDropsBin)) {
-        New-Item -Path $qirDropsBin -ItemType "directory"
     }
     if (-not (Test-Path $qirDropsInclude)) {
         New-Item -Path $qirDropsInclude -ItemType "directory"
     }
     $qirBinaries = (Join-Path $PSScriptRoot .. .. .. target "$Env:BUILD_CONFIGURATION".ToLower() *)
-    Copy-Item $qirBinaries $qirDropsBin -Include "*qir_stdlib*" -Exclude "*.rlib","*.d","*.exp"
+    Copy-Item $qirBinaries $Env:QIR_DROPS -Include "*qir_stdlib*" -Exclude "*.rlib","*.d","*.exp"
 
     # Copy the C API header and def file
-    Copy-Item (Join-Path $PSScriptRoot stdlib include qir_stdlib.h) (Join-Path $Env:QIR_DROPS include)
-    Copy-Item (Join-Path $PSScriptRoot stdlib include qir_stdlib.def) (Join-Path $Env:QIR_DROPS include)
+    $qirIncludes = (Join-Path $PSScriptRoot .. .. .. target "$Env:BUILD_CONFIGURATION".ToLower() build include *)
+    Copy-Item $qirIncludes $qirDropsInclude
 
     # When building in CI, free disk space by cleaning up.
     # Note that this takes longer, but saves ~1 GB of space.
--- src/Qir/Runtime/stdlib/build.rs
+++ src/Qir/Runtime/stdlib/build.rs
@@ -1,59 +1,37 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-use std::env;
 use std::path::PathBuf;
-use std::process::Command;
-
-use llvm_tools::LlvmTools;
+use std::{env, fs};
 
 fn main() -> Result<(), String> {
-    // Compile the LLVM IR bridge file. Requires the llvm-tools-preview component.
-    // This is only needed for range support, and this entire build.rs can be dropped when that functionality is
-    // no longer needed.
+    let include_dir = env::var_os("DEP_QIR_STDLIB_INCLUDE")
+        .map(PathBuf::from)
+        .ok_or_else(|| "Environment variable DEP_QIR-STDLIB_INCLUDE not defined.".to_string())?;
+
     let out_dir = env::var_os("OUT_DIR")
         .map(PathBuf::from)
         .ok_or_else(|| "Environment variable OUT_DIR not defined.".to_string())?;
-
-    let llvm_tools = LlvmTools::new().map_err(|err| {
+    let include_dest_dir = out_dir.join("..").join("..").join("include");
+    fs::create_dir_all(&include_dest_dir).map_err(|err| {
         format!(
-            "Failed to locate llvm tools: {:?}. Is the llvm-tools-preview component installed? Try using `rustup component add llvm-tools-preview`.",
+            "Unable to create destination folder for include files: {}",
             err
         )
     })?;
 
-    let llc_path = llvm_tools
-        .tool(llvm_tools::exe("llc").to_string().as_str())
-        .ok_or_else(|| "Failed to find llc.".to_string())?;
-    let llvm_ar_path = llvm_tools
-        .tool(llvm_tools::exe("llvm-ar").to_string().as_str())
-        .ok_or_else(|| "Failed to find llvm-ar.".to_string())?;
-    let lib_name = if cfg!(target_os = "windows") {
-        "bridge-rt.lib"
-    } else {
-        "libbridge-rt.a"
-    };
-
-    Command::new(llc_path)
-        .args(&[
-            "--filetype=obj",
-            "./src/bridge-rt.ll",
-            "-o",
-            &format!("{}/bridge-rt.o", out_dir.display()),
-        ])
-        .status()
-        .map_err(|err| format!("llc failed: {}.", err))?;
-    Command::new(llvm_ar_path)
-        .args(&[
-            "-r",
-            &format!("{}/{}", out_dir.display(), lib_name),
-            &format!("{}/bridge-rt.o", out_dir.display()),
-        ])
-        .status()
-        .map_err(|err| format!("llvm-ar failed: {}.", err))?;
-
-    println!("cargo:rustc-link-lib=static=bridge-rt");
-    println!("cargo:rustc-link-search=native={}", out_dir.display());
+    let include_files = fs::read_dir(include_dir)
+        .map_err(|err| format!("Unable to read files in include directory: {}", err))?;
+    for file in include_files {
+        let file = file.unwrap();
+        fs::copy(file.path(), include_dest_dir.join(file.file_name())).map_err(|err| {
+            format!(
+                "Failed to copy file '{}': {}",
+                file.file_name().to_str().unwrap(),
+                err
+            )
+        })?;
+    }
 
     Ok(())
 }
