3cfcb766025ce84dd15898586916039911c93232
Alex Hansen <alehansen@microsoft.com>
2023-08-29 11:27:18 -0700

    `rustup toolchain install` and `rustup install` are the same thing. Also, removes extra toolchain installs.

--- src/Simulation/qdk_sim_rs/prerequisites.ps1
+++ src/Simulation/qdk_sim_rs/prerequisites.ps1
@@ -20,9 +20,5 @@ if (-not (Get-Command rustup -ErrorAction SilentlyContinue)) {
 
 # Now that rustup is available, go on and make sure that nightly support for
 # rustfmt and clippy is available.
-rustup install nightly
-rustup toolchain install nightly
 rustup toolchain install nightly-2022-08-01
-rustup component add rustfmt clippy llvm-tools-preview
-rustup component add rustfmt clippy llvm-tools-preview --toolchain nightly
 rustup component add rustfmt clippy llvm-tools-preview --toolchain nightly-2022-08-01
