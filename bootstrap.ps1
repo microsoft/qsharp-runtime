edbdd356e807164f2fbf4effcd972ea9ba037e16
Stefan J. Wernli <swernli@microsoft.com>
2023-10-26 10:30:45 -0700

    don't use cargo edit

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -6,20 +6,21 @@
 $ErrorActionPreference = 'Stop'
 
 Push-Location (Join-Path $PSScriptRoot "build")
-    .\prerequisites.ps1
+.\prerequisites.ps1
 Pop-Location
 
-cargo install cargo-edit@0.11.0
-Push-Location (Join-Path $PSScriptRoot "./src/Simulation/qdk_sim_rs")
-    cargo set-version $Env:NUGET_VERSION;
-Pop-Location
+# cargo install cargo-edit@0.11.0
+# Push-Location (Join-Path $PSScriptRoot "./src/Simulation/qdk_sim_rs")
+#     cargo set-version $Env:NUGET_VERSION;
+# Pop-Location
 
-if (-not (Test-Path Env:/AGENT_OS)) {                                    # If not CI build, i.e. local build (if AGENT_OS envvar is not defined)
+if (-not (Test-Path Env:/AGENT_OS)) {
+    # If not CI build, i.e. local build (if AGENT_OS envvar is not defined)
     if ($Env:ENABLE_QIRRUNTIME -ne "false") {
         Write-Host "Build release flavor of the QIR standard library"
         $Env:BUILD_CONFIGURATION = "Release"
         Push-Location (Join-Path $PSScriptRoot "src/Qir/Runtime")
-            .\build-qir-stdlib.ps1
+        .\build-qir-stdlib.ps1
         Pop-Location
         $Env:BUILD_CONFIGURATION = $null
     }
@@ -27,21 +28,22 @@ if (-not (Test-Path Env:/AGENT_OS)) {                                    # If no
         $Env:BUILD_CONFIGURATION = "Release"
         Write-Host "Build release flavor of the full state simulator"
         Push-Location (Join-Path $PSScriptRoot "src/Simulation/Native")
-            .\build-native-simulator.ps1
+        .\build-native-simulator.ps1
         Pop-Location
 
         Write-Host "Build release flavor of the Sparse Simulator"
         & (Join-Path $PSScriptRoot "src" "Simulation" "NativeSparseSimulator" "build.ps1")
 
         Push-Location (Join-Path $PSScriptRoot "src" "Simulation" "qdk_sim_rs")
-            # Don't run the experimental simulator build if we're local
-            # and prerequisites are missing.
-            $IsCI = "$Env:TF_BUILD" -ne "" -or "$Env:CI" -eq "true";
-            if ((Get-Command cargo -ErrorAction SilentlyContinue) -or $IsCI) {
-                .\build-qdk-sim-rs.ps1
-            } else {
-                Write-Verbose "cargo was not installed, skipping qdk_sim_rs build.";
-            }
+        # Don't run the experimental simulator build if we're local
+        # and prerequisites are missing.
+        $IsCI = "$Env:TF_BUILD" -ne "" -or "$Env:CI" -eq "true";
+        if ((Get-Command cargo -ErrorAction SilentlyContinue) -or $IsCI) {
+            .\build-qdk-sim-rs.ps1
+        }
+        else {
+            Write-Verbose "cargo was not installed, skipping qdk_sim_rs build.";
+        }
         Pop-Location
         $Env:BUILD_CONFIGURATION = $null
     }
